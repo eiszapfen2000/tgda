3bc8a5d47d10887f9cb822f8492342c58a1f40c7
Thomas Gamper <1222479+eiszapfen2000@users.noreply.github.com>
2007-09-13 15:52:04 +0000

    TG: RNG manager added

---
+++ src/RandomNumbers/NPRandomNumberGeneratorsManager.h
@@ -0,0 +1,71 @@
+#import <Foundation/Foundation.h>
+
+#import "NPPRandomNumberGeneration.h"
+#import "NPRandomNumberGenerators.h"
+
+@interface NPRandomNumberGeneratorsManager : NSObject
+{
+	NSSet * fixedParameterGeneratorNames;
+
+	NSMutableArray * generators;
+}
+
+- init;
+
+- (NPRandomNumberGenerator *) fixedParameterGeneratorWithString: (NSString *) name;
+
+- (NPRandomNumberGenerator *) mersenneTwisterWithSeed: (ULong) seed;
+
+- (NPRandomNumberGenerator *) lcgGenerator;
+- (NPRandomNumberGenerator *) lcgGeneratorWithParameters
+    : (ULong) periodLength
+    : (ULong) seed
+    : (ULong) a
+    : (ULong) b
+    ;
+
+
+- (NPRandomNumberGenerator *) icgGenerator;
+- (NPRandomNumberGenerator *) icgGeneratorWithParameters
+    : (ULong) periodLength
+    : (ULong) seed
+    : (ULong) a
+    : (ULong) b
+    ;
+
+- (NPRandomNumberGenerator *) eicgGenerator;
+- (NPRandomNumberGenerator *) eicgGeneratorWithParameters
+    : (ULong) periodLength
+    : (ULong) seed
+    : (ULong) a
+    : (ULong) b
+    ;
+
+- (NPRandomNumberGenerator *) meicgGenerator;
+- (NPRandomNumberGenerator *) meicgGeneratorWithParameters
+    : (ULong) periodLength
+    : (ULong) seed
+    : (ULong) a
+    : (ULong) b
+    ;
+
+- (NPRandomNumberGenerator *) dicgGenerator;
+- (NPRandomNumberGenerator *) dicgGeneratorWithParameters
+    : (ULong) periodExponent
+    : (ULong) seed
+    : (ULong) a
+    : (ULong) b
+    ;
+
+- (NPRandomNumberGenerator *) qcgGenerator;
+- (NPRandomNumberGenerator *) qcgGeneratorWithParameters
+    : (ULong) periodLength
+    : (ULong) seed
+    : (ULong) a
+    : (ULong) b
+    : (ULong) c
+    ;
+
+- (void) dealloc;
+
+@end
