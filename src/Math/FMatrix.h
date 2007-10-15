d36abbed379fa732d1dac1200c37fe0936aefe75
Thomas Gamper <1222479+eiszapfen2000@users.noreply.github.com>
2007-10-15 13:02:01 +0000

    TG: some matrix updates

--- src/Math/FMatrix.h
+++ src/Math/FMatrix.h
@@ -28,20 +28,20 @@ void fm2_m_set_identity(FMatrix2 * m);
 void fm3_m_set_identity(FMatrix3 * m);
 void fm4_m_set_identity(FMatrix4 * m);
 
-void fm2_m_transpose_m(FMatrix2 * m, FMatrix2 * transpose);
-void fm3_m_transpose_m(FMatrix3 * m, FMatrix3 * transpose);
-void fm4_m_transpose_m(FMatrix4 * m, FMatrix4 * transpose);
+void fm2_m_transpose_m(const FMatrix2 * m, FMatrix2 * transpose);
+void fm3_m_transpose_m(const FMatrix3 * m, FMatrix3 * transpose);
+void fm4_m_transpose_m(const FMatrix4 * m, FMatrix4 * transpose);
 
-void fm2_mm_multiply_m(FMatrix2 * m1, FMatrix2 * m2, FMatrix2 * result);
-void fm3_mm_multiply_m(FMatrix3 * m1, FMatrix3 * m2, FMatrix3 * result);
-void fm4_mm_multiply_m(FMatrix4 * m1, FMatrix4 * m2, FMatrix4 * result);
+void fm2_mm_multiply_m(const FMatrix2 * m1, const FMatrix2 * m2, FMatrix2 * result);
+void fm3_mm_multiply_m(const FMatrix3 * m1, const FMatrix3 * m2, FMatrix3 * result);
+void fm4_mm_multiply_m(const FMatrix4 * m1, const FMatrix4 * m2, FMatrix4 * result);
 
-void fm2_mm_add_m(FMatrix2 * m1, FMatrix2 * m2, FMatrix2 * result);
-void fm3_mm_add_m(FMatrix3 * m1, FMatrix3 * m2, FMatrix3 * result);
-void fm4_mm_add_m(FMatrix4 * m1, FMatrix4 * m2, FMatrix4 * result);
+void fm2_mm_add_m(const FMatrix2 * m1, const FMatrix2 * m2, FMatrix2 * result);
+void fm3_mm_add_m(const FMatrix3 * m1, const FMatrix3 * m2, FMatrix3 * result);
+void fm4_mm_add_m(const FMatrix4 * m1, const FMatrix4 * m2, FMatrix4 * result);
 
-void fm2_mm_subtract_m(FMatrix2 * m1, FMatrix2 * m2, FMatrix2 * result);
-void fm3_mm_subtract_m(FMatrix3 * m1, FMatrix3 * m2, FMatrix3 * result);
-void fm4_mm_subtract_m(FMatrix4 * m1, FMatrix4 * m2, FMatrix4 * result);
+void fm2_mm_subtract_m(const FMatrix2 * m1, const FMatrix2 * m2, FMatrix2 * result);
+void fm3_mm_subtract_m(const FMatrix3 * m1, const FMatrix3 * m2, FMatrix3 * result);
+void fm4_mm_subtract_m(const FMatrix4 * m1, const FMatrix4 * m2, FMatrix4 * result);
 
 #endif
