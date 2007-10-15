d1a413303b20ca5b9739716c7a85358a2fee7e8a
Thomas Gamper <1222479+eiszapfen2000@users.noreply.github.com>
2007-10-15 09:16:50 +0000

    TG: some matrix updates

--- src/Math/Matrix.c
+++ src/Math/Matrix.c
@@ -1,5 +1,71 @@
 #include "Matrix.h"
 
+void m2_m_set_identity(Matrix2 * m)
+{
+    M_E(*m,0,0) = M_E(*m,1,1) = 1.0;
+    M_E(*m,0,1) = M_E(*m,1,0) = 0.0;
+}
+
+void m3_m_set_identity(Matrix3 * m)
+{
+    M_E(*m,0,0) = M_E(*m,1,1) = M_E(*m,2,2) = 1.0;
+    M_E(*m,0,1) = M_E(*m,0,2) = M_E(*m,1,0) = M_E(*m,1,2) = M_E(*m,2,0) = M_E(*m,2,1) = 0.0;
+}
+
+void m4_m_set_identity(Matrix4 * m)
+{
+    M_E(*m,0,0) = M_E(*m,1,1) = M_E(*m,2,2) = M_E(*m,3,3) = 1.0;
+    M_E(*m,0,1) = M_E(*m,0,2) = M_E(*m,0,3) = M_E(*m,1,0) = M_E(*m,1,2) = M_E(*m,1,3) =
+    M_E(*m,2,0) = M_E(*m,2,1) = M_E(*m,2,3) = M_E(*m,3,0) = M_E(*m,3,1) = M_E(*m,3,2) = 0.0;
+}
+
+void m2_m_transpose_m(Matrix2 * m, Matrix2 * transpose)
+{
+    M_E(*transpose,0,0) = M_E(*m,0,0);
+    M_E(*transpose,0,1) = M_E(*m,1,0);
+
+    M_E(*transpose,1,0) = M_E(*m,0,1);
+    M_E(*transpose,1,1) = M_E(*m,1,1);
+}
+
+void m3_m_transpose_m(Matrix3 * m, Matrix3 * transpose)
+{
+    M_E(*transpose,0,0) = M_E(*m,0,0);
+    M_E(*transpose,0,1) = M_E(*m,1,0);
+    M_E(*transpose,0,2) = M_E(*m,2,0);
+
+    M_E(*transpose,1,0) = M_E(*m,0,1);
+    M_E(*transpose,1,1) = M_E(*m,1,1);
+    M_E(*transpose,1,2) = M_E(*m,2,1);
+
+    M_E(*transpose,2,0) = M_E(*m,0,2);
+    M_E(*transpose,2,1) = M_E(*m,1,2);
+    M_E(*transpose,2,2) = M_E(*m,2,2);
+}
+
+void m4_m_transpose_m(Matrix4 * m, Matrix4 * transpose)
+{
+    M_E(*transpose,0,0) = M_E(*m,0,0);
+    M_E(*transpose,0,1) = M_E(*m,1,0);
+    M_E(*transpose,0,2) = M_E(*m,2,0);
+    M_E(*transpose,0,3) = M_E(*m,3,0);
+
+    M_E(*transpose,1,0) = M_E(*m,0,1);
+    M_E(*transpose,1,1) = M_E(*m,1,1);
+    M_E(*transpose,1,2) = M_E(*m,2,1);
+    M_E(*transpose,1,3) = M_E(*m,3,1);
+
+    M_E(*transpose,2,0) = M_E(*m,0,2);
+    M_E(*transpose,2,1) = M_E(*m,1,2);
+    M_E(*transpose,2,2) = M_E(*m,2,2);
+    M_E(*transpose,2,3) = M_E(*m,3,2);
+
+    M_E(*transpose,3,0) = M_E(*m,0,3);
+    M_E(*transpose,3,1) = M_E(*m,1,3);
+    M_E(*transpose,3,2) = M_E(*m,2,3);
+    M_E(*transpose,3,3) = M_E(*m,3,3);
+}
+
 void m2_mm_multiply_m(Matrix2 * m1, Matrix2 * m2, Matrix2 * result)
 {
     M_E(*result,0,0) = M_E(*m1,0,0)*M_E(*m2,0,0) + M_E(*m1,1,0)*M_E(*m2,0,1);
