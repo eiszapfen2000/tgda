bfa38c3be07e7dd1510ef6bb7f5dbdf21e9a116c
icicle <icicle@f832cba2-7736-0410-a208-86f5be76f6c5>
2007-11-07 16:48:23 +0000

    TG: image lib

---
+++ src/Image/NPDevilTexture.m
@@ -0,0 +1,41 @@
+#import "NPDevilTexture.h"
+
+void npimage_initialise()
+{
+    ilInit();
+    iluInit();
+    ilutInit();
+}
+
+@implementation NPDevilTexture
+
++ (void) initIlutGL
+{
+    ilutRenderer(ILUT_OPENGL);
+}
+
++ (GLuint) glTextureFromFile: (NSString *) fileName;
+{
+    if ((ilGetInteger(IL_VERSION_NUM) < IL_VERSION) ||
+        (iluGetInteger(ILU_VERSION_NUM) < ILU_VERSION) ||
+        (ilutGetInteger(ILUT_VERSION_NUM) < ILUT_VERSION)) 
+    {
+        NSLog(@"Devil wrong version");
+    }
+
+    ILuint ilImageHandle;
+
+    ilGenImages(1, &ilImageHandle);
+    ilBindImage(ilImageHandle);
+    const char * brak = [ fileName cString ];
+    ilLoadImage(brak);
+    iluFlipImage();
+
+    GLuint glImageHandle = ilutGLBindMipmaps();
+
+    ilDeleteImages(1, &ilImageHandle);
+
+    return glImageHandle;
+}
+
+@end
