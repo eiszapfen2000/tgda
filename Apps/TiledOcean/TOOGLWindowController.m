f87a1b477fbcbd04a4737bcd731c8ea635ddbf2f
Thomas Gamper <1222479+eiszapfen2000@users.noreply.github.com>
2008-02-14 22:54:27 +0000

    TG: initial run loop test

--- Apps/TiledOcean/TOOGLWindowController.m
+++ Apps/TiledOcean/TOOGLWindowController.m
@@ -4,7 +4,26 @@
 
 - init
 {
-	return [ super initWithWindowNibName: @"TODocument" ];
+	return  [ super initWithWindowNibName: @"TODocument" ];
+}
+
+- (void) windowDidLoad
+{
+    NSLog(@"didload");
+
+      timer = [NSTimer scheduledTimerWithTimeInterval:0.015
+                                               target:self
+                                             selector:@selector(doDrawingStuff)
+                                             userInfo: nil
+                                              repeats: YES ];
+}
+
+- (void) doDrawingStuff
+{
+    [ openglView lockFocus ];
+//    [ openglView display ];
+    [ openglView drawRect:[openglView frame] ];
+    [ openglView unlockFocus ];
 }
 
 @end
