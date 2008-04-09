4f728fc31fbad7a507cc3f2953e8df4006cb5ec7
Thomas Gamper <1222479+eiszapfen2000@users.noreply.github.com>
2008-04-09 11:33:28 +0000

    TG: stuff

---
+++ NPEngine/Core/Math/NpMath.h
@@ -0,0 +1,17 @@
+#ifndef _NP_MATH_MATH_H_
+#define _NP_MATH_MATH_H_
+
+#include "Constants.h"
+#include "Utilities.h"
+
+#include "FMatrix.h"
+#include "FQuaternion.h"
+#include "FVector.h"
+
+#include "Matrix.h"
+#include "Quaternion.h"
+#include "Vector.h"
+
+void npmath_initialise();
+
+#endif
