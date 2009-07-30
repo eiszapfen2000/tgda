74e6a0e2e63a2e441b65433554270d94d87b5ec4
Thomas Gamper <1222479+eiszapfen2000@users.noreply.github.com>
2009-07-30 21:34:54 +0000

    TG: NPTexture3D improvements

--- NPEngine/Core/Math/IVector.c
+++ NPEngine/Core/Math/IVector.c
@@ -52,3 +52,49 @@ IVector2 * iv2_free(IVector2 * v)
 {
     return npfreenode_free(v, NP_IVECTOR2_FREELIST);
 }
+
+
+IVector3 * iv3_alloc()
+{
+    return (IVector3 *)npfreenode_alloc(NP_IVECTOR3_FREELIST);
+}
+
+IVector3 * iv3_alloc_init()
+{
+    IVector3 * tmp = npfreenode_alloc(NP_IVECTOR3_FREELIST);
+    V_X(*tmp) = V_Y(*tmp) = V_Z(*tmp) = 0;
+
+    return tmp;
+}
+
+IVector3 * iv3_alloc_init_with_iv2(IVector3 * v)
+{
+    IVector3 * tmp = npfreenode_alloc(NP_IVECTOR3_FREELIST);
+    V_X(*tmp) = V_X(*v);
+    V_Y(*tmp) = V_Y(*v);
+    V_Z(*tmp) = V_Z(*v);
+
+    return tmp;
+}
+
+IVector3 * iv3_alloc_init_with_components(Int x, Int y, Int z)
+{
+    IVector3 * tmp = npfreenode_alloc(NP_IVECTOR3_FREELIST);
+    V_X(*tmp) = x;
+    V_Y(*tmp) = y;
+    V_Z(*tmp) = z;
+
+    return tmp;
+}
+
+void iv3_v_copy_v(IVector3 * source, IVector3 * target)
+{
+    V_X(*source) = V_X(*target);
+    V_Y(*source) = V_Y(*target);
+    V_Z(*source) = V_Z(*target);
+}
+
+IVector3 * iv3_free(IVector3 * v)
+{
+    return npfreenode_free(v, NP_IVECTOR3_FREELIST);
+}
