a453193e1f73dbb3014edf085b4cf9531dbc4644
icicle <icicle@f832cba2-7736-0410-a208-86f5be76f6c5>
2009-11-10 22:01:57 +0000

    TG: Add missing files

---
+++ NPEngine/Graphics/Material/NPEffectTechnique.h
@@ -0,0 +1,17 @@
+#import "Core/NPObject/NPObject.h"
+#import "Graphics/npgl.h"
+
+@interface NPEffectTechnique : NPObject
+{
+    CGtechnique technique;
+}
+
+- (id) init;
+- (id) initWithParent:(id <NPPObject> )newParent;
+- (id) initWithName:(NSString *)newName parent:(id <NPPObject> )newParent;
+- (id) initWithName:(NSString *)newName parent:(id <NPPObject> )newParent technique:(CGtechnique)newTechnique;
+- (void) dealloc;
+
+- (CGpass) firstPass;
+
+@end
