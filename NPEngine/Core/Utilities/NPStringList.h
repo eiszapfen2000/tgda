0143a34008057db603d3cb0dcf3490c415ebf7fc
Thomas Gamper <1222479+eiszapfen2000@users.noreply.github.com>
2010-05-18 14:11:43 +0000

    TG: NPStringList re-implemented

--- NPEngine/Core/Utilities/NPStringList.h
+++ NPEngine/Core/Utilities/NPStringList.h
@@ -3,13 +3,24 @@
 
 @interface NPStringList : NPObject
 {
-    NSArray * lines;
+    NSMutableArray * lines;
+
+    BOOL allowDuplicates;
+    BOOL allowEmptyStrings;
 }
 
 - (id) init;
 - (id) initWithName:(NSString *)newName;
 - (id) initWithName:(NSString *)newName parent:(id <NPPObject> )newParent;
+- (id) initWithName:(NSString *)newName
+             parent:(id <NPPObject> )newParent
+    allowDuplicates:(BOOL)newAllowDuplicates
+  allowEmptyStrings:(BOOL)newAllowEmptyStrings
+                   ;
 - (void) dealloc;
+
+- (void) clear;
+- (void) addString:(NSString *)string;
 
 - (BOOL) loadFromPath:(NSString *)path;
 
