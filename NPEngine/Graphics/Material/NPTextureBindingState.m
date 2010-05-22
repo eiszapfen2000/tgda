3e0113d4b8a00730e889aa51b6678ffc2ada230a
Thomas Gamper <1222479+eiszapfen2000@users.noreply.github.com>
2010-05-22 10:09:37 +0000

    TG: NPTextureBindingState - remove texelunit array; NPEffectTechnique - formatting; NPSUXMaterialInstance + Compiler - minor cleanups and bugfixes

--- NPEngine/Graphics/Material/NPTextureBindingState.m
+++ NPEngine/Graphics/Material/NPTextureBindingState.m
@@ -21,37 +21,16 @@
     self = [ super initWithName:newName parent:newParent ];
 
     textureBindings = [[ NSMutableDictionary alloc ] init ];
-    textureUnits = [[ NSMutableArray alloc ] initWithCapacity:NP_GRAPHICS_SAMPLER_COUNT ];
-
-    for ( Int i = 0; i < NP_GRAPHICS_SAMPLER_COUNT; i++ )
-    {
-        [ textureUnits addObject:[NSNull null] ];
-    }
 
     return self;
 }
 
 - (void) dealloc
 {
-    [ textureUnits removeAllObjects ];
-    [ textureUnits release ];
     [ textureBindings removeAllObjects ];
     [ textureBindings release ];
 
     [ super dealloc ];
-}
-
-- (void) clear
-{
-    [ textureUnits removeAllObjects ];
-
-    for ( Int i = 0; i < NP_GRAPHICS_SAMPLER_COUNT; i++ )
-    {
-        [ textureUnits replaceObjectAtIndex:i withObject:[NSNull null] ];
-
-        glActiveTexture(GL_TEXTURE0 + i);
-        glBindTexture(GL_TEXTURE_2D, 0);       
-    }
 }
 
 - (id) textureForKey:(NSString *)colormapSemantic
