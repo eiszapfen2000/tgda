0a8507b5db1a477c923fd5709ebfcf5c394cd51b
Thomas Gamper <1222479+eiszapfen2000@users.noreply.github.com>
2011-07-23 15:34:44 +0000

    TG: ODPerlinNoise - reorganise some code

--- AppsV2/OceanDemo/main.m
+++ AppsV2/OceanDemo/main.m
@@ -66,7 +66,7 @@ int main (int argc, char **argv)
     NSAutoreleasePool * pool = [ NSAutoreleasePool new ];
 
     ODPerlinNoise * noise = [[ ODPerlinNoise alloc ] init ];
-    [ noise generateWithSize:4 ];
+    [ noise generate ];
     DESTROY(noise);
 
     // Initialise GLFW
