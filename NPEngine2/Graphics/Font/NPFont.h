749339b3cb4c244476b3ea2d67c13cb8b1849ff8
Thomas Gamper <1222479+eiszapfen2000@users.noreply.github.com>
2011-07-31 21:37:47 +0000

    TG: NPFont - add setters and getters, initial loading facility; NPFontCompiler - load info and common section of fnt file

--- NPEngine2/Graphics/Font/NPFont.h
+++ NPEngine2/Graphics/Font/NPFont.h
@@ -8,12 +8,34 @@
     NSString * file;
     BOOL ready;
 
+    NSString * fontFaceName;
+    int32_t renderedSize;
+    int32_t lineHeight;
+    int32_t baseLine;
+    int32_t textureWidth;
+    int32_t textureHeight;
     NSMutableArray * characterPages;
 }
 
 - (id) init;
 - (id) initWithName:(NSString *)newName;
 - (void) dealloc;
+
+- (void) clear;
+
+- (NSString *) fontFaceName;
+- (int32_t) renderedSize;
+- (int32_t) lineHeight;
+- (int32_t) baseLine;
+- (int32_t) textureWidth;
+- (int32_t) textureHeight;
+
+- (void) setFontFaceName:(NSString *)newFontFaceName;
+- (void) setRenderedSize:(const int32_t)newRenderedSize;
+- (void) setLineHeight:(const int32_t)newLineHeight;
+- (void) setBaseLine:(const int32_t)newBaseLine;
+- (void) setTextureWidth:(const int32_t)newTextureWidth;
+- (void) setTextureHeight:(const int32_t)newTextureHeight;
 
 @end
 
