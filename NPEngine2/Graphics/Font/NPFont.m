bc05ca5d275c29c73af0a0c3123292d77802e37d
icicle <icicle@f832cba2-7736-0410-a208-86f5be76f6c5>
2011-07-31 21:37:47 +0000

    TG: NPFont - add setters and getters, initial loading facility; NPFontCompiler - load info and common section of fnt file

--- NPEngine2/Graphics/Font/NPFont.m
+++ NPEngine2/Graphics/Font/NPFont.m
@@ -1,5 +1,33 @@
 #import <Foundation/NSArray.h>
+#import <Foundation/NSException.h>
+#import "Log/NPLog.h"
+#import "Core/Container/NSArray+NPPObject.h"
+#import "Core/Utilities/NSError+NPEngine.h"
+#import "Core/String/NPStringList.h"
+#import "Core/NPEngineCore.h"
+#import "NPFontCompiler.h"
 #import "NPFont.h"
+
+@interface NPFont (Private)
+
+- (void) loadFromStringList:(NPStringList *)stringList;
+
+@end
+
+@implementation NPFont (Private)
+
+- (void) loadFromStringList:(NPStringList *)stringList
+{
+    NSAssert(stringList != nil, @"");
+
+    NPFontCompiler * compiler = [[ NPFontCompiler alloc ] init ];
+    [ compiler compileScript:stringList intoFont:self ];
+    DESTROY(compiler);
+
+    ready = YES;
+}
+
+@end
 
 @implementation NPFont
 
@@ -15,6 +43,10 @@
     file = nil;
     ready = NO;
 
+    fontFaceName = nil;
+    renderedSize = 0;
+    lineHeight = baseLine = 0;
+    textureWidth = textureHeight = 0;
     characterPages = [[ NSMutableArray alloc ] init ];
 
     return self;
@@ -29,6 +61,18 @@
     [ super dealloc ];
 }
 
+- (void) clear
+{
+    SAFE_DESTROY(file);
+    ready = NO;
+
+    SAFE_DESTROY(fontFaceName);
+    [ characterPages removeAllObjects ];
+    renderedSize = 0;
+    lineHeight = baseLine = 0;
+    textureWidth = textureHeight = 0;
+}
+
 - (BOOL) ready
 {
     return ready;
@@ -37,6 +81,66 @@
 - (NSString *) fileName
 {
     return file;
+}
+
+- (NSString *) fontFaceName
+{
+    return fontFaceName;
+}
+
+- (int32_t) renderedSize
+{
+    return renderedSize;
+}
+
+- (int32_t) lineHeight
+{
+    return lineHeight;
+}
+
+- (int32_t) baseLine
+{
+    return baseLine;
+}
+
+- (int32_t) textureWidth
+{
+    return textureWidth;
+}
+
+- (int32_t) textureHeight
+{
+    return textureHeight;
+}
+
+- (void) setFontFaceName:(NSString *)newFontFaceName
+{
+    ASSIGNCOPY(fontFaceName, newFontFaceName);
+}
+
+- (void) setRenderedSize:(const int32_t)newRenderedSize
+{
+    renderedSize = newRenderedSize;
+}
+
+- (void) setLineHeight:(const int32_t)newLineHeight
+{
+    lineHeight = newLineHeight;
+}
+
+- (void) setBaseLine:(const int32_t)newBaseLine
+{
+    baseLine = newBaseLine;
+}
+
+- (void) setTextureWidth:(const int32_t)newTextureWidth
+{
+    textureWidth = newTextureWidth;
+}
+
+- (void) setTextureHeight:(const int32_t)newTextureHeight
+{
+    textureHeight = newTextureHeight;
 }
 
 - (BOOL) loadFromStream:(id <NPPStream>)stream 
@@ -49,7 +153,44 @@
             arguments:(NSDictionary *)arguments
                 error:(NSError **)error
 {
-    return NO;
+
+    [ self clear ];
+
+    // check if file is to be found
+    NSString * completeFileName
+        = [[[ NPEngineCore instance ] localPathManager ] getAbsolutePath:fileName ];
+
+    if ( completeFileName == nil )
+    {
+        if ( error != NULL )
+        {
+            *error = [ NSError fileNotFoundError:fileName ];
+        }
+
+        return NO;
+    }
+
+    [ self setName:completeFileName ];
+    ASSIGNCOPY(file, completeFileName);
+
+    NPLOG(@"Loading font \"%@\"", completeFileName);
+
+    NPStringList * fontScript
+        = AUTORELEASE([[ NPStringList alloc ]
+                            initWithName:@"" 
+                         allowDuplicates:YES
+                       allowEmptyStrings:NO ]);
+
+    if ( [ fontScript loadFromFile:completeFileName
+                         arguments:nil
+                             error:error ] == NO )
+    {
+        return NO;
+    }
+
+    [ self loadFromStringList:fontScript ];
+
+    return YES;
 }
 
 @end
--- NPEngine2/Graphics/Font/NPFontCompiler.m
+++ NPEngine2/Graphics/Font/NPFontCompiler.m
@@ -5,28 +5,75 @@
 
 @interface NPFontCompiler (Private)
 
-- (void) parseInfo;
-- (void) parseCommon;
-- (void) parsePages;
-- (void) parseCharacters;
+- (void) parseInfoAtLine:(const NSUInteger)lineIndex;
+- (void) parseCommonAtLine:(const NSUInteger)lineIndex;
+- (void) parsePageAtLine:(const NSUInteger)lineIndex;
+- (void) parseCharactersStartingAtLine:(const NSUInteger)lineIndex;
 
 @end
 
-@implementation NPFont (Private)
+@implementation NPFontCompiler (Private)
 
-- (void) parseInfo
+- (void) parseInfoAtLine:(const NSUInteger)lineIndex
+{
+    NSString * fontFaceName = nil;
+    if ( [ self isLowerCaseTokenFromLine:lineIndex atPosition:1 equalToString:@"face" ] == YES
+         && [ self isTokenFromLine:lineIndex atPosition:2 equalToString:@"=" ] == YES
+         && [ self getTokenAsString:&fontFaceName fromLine:lineIndex atPosition:3 ] == YES )
+    {
+        [ font setFontFaceName:fontFaceName ];
+    }
+
+    int32_t renderedSize = 0;
+    if ( [ self isLowerCaseTokenFromLine:lineIndex atPosition:4 equalToString:@"size" ] == YES
+         && [ self isTokenFromLine:lineIndex atPosition:5 equalToString:@"=" ] == YES
+         && [ self getTokenAsInt:&renderedSize fromLine:lineIndex atPosition:6 ] == YES )
+    {
+        [ font setRenderedSize:renderedSize ];
+    }
+}
+        
+
+- (void) parseCommonAtLine:(const NSUInteger)lineIndex
+{
+    int32_t lineHeight = -1;
+    if ( [ self isLowerCaseTokenFromLine:lineIndex atPosition:1 equalToString:@"lineheight" ] == YES
+         && [ self isTokenFromLine:lineIndex atPosition:2 equalToString:@"=" ] == YES
+         && [ self getTokenAsInt:&lineHeight fromLine:lineIndex atPosition:3 ] == YES )
+    {
+        [ font setLineHeight:lineHeight ];
+    }
+
+    int32_t baseLine = -1;
+    if ( [ self isLowerCaseTokenFromLine:lineIndex atPosition:4 equalToString:@"base" ] == YES
+         && [ self isTokenFromLine:lineIndex atPosition:5 equalToString:@"=" ] == YES
+         && [ self getTokenAsInt:&baseLine fromLine:lineIndex atPosition:6 ] == YES )
+    {
+        [ font setBaseLine:baseLine ];
+    }
+
+    int32_t textureWidth = -1;
+    if ( [ self isLowerCaseTokenFromLine:lineIndex atPosition:7 equalToString:@"scalew" ] == YES
+         && [ self isTokenFromLine:lineIndex atPosition:8 equalToString:@"=" ] == YES
+         && [ self getTokenAsInt:&textureWidth fromLine:lineIndex atPosition:9 ] == YES )
+    {
+        [ font setTextureWidth:textureWidth ];
+    }
+
+    int32_t textureHeight = -1;
+    if ( [ self isLowerCaseTokenFromLine:lineIndex atPosition:10 equalToString:@"scaleh" ] == YES
+         && [ self isTokenFromLine:lineIndex atPosition:11 equalToString:@"=" ] == YES
+         && [ self getTokenAsInt:&textureHeight fromLine:lineIndex atPosition:12 ] == YES )
+    {
+        [ font setTextureHeight:textureHeight ];
+    }
+}
+
+- (void) parsePageAtLine:(const NSUInteger)lineIndex
 {
 }
 
-- (void) parseCommon
-{
-}
-
-- (void) parsePages
-{
-}
-
-- (void) parseCharacters
+- (void) parseCharactersStartingAtLine:(const NSUInteger)lineIndex
 {
 }
 
@@ -42,10 +89,36 @@
     ASSIGN(font, targetFont);
     [ self parse:inputScript ];
 
-    [ self parseInfo ];
-    [ self parseCommon ];
-    [ self parsePages ];
-    [ self parseCharacters ];
+    const NSUInteger numberOfLines = [ inputScript count ];
+    for ( NSUInteger i = 0; i < numberOfLines; i++ )
+    {
+        NSString * token = nil;
+        if ( [ self getTokenAsLowerCaseString:&token
+                                     fromLine:i
+                                   atPosition:0 ] == YES )
+        {
+            if ( [ token isEqualToString:@"info" ] == YES )
+            {
+                [ self parseInfoAtLine:i ];
+            }
+
+            if ( [ token isEqualToString:@"common" ] == YES )
+            {
+                [ self parseCommonAtLine:i ];
+            }
+
+            if ( [ token isEqualToString:@"page" ] == YES )
+            {
+                [ self parsePageAtLine:i ];
+            }
+
+            if ( [ token isEqualToString:@"chars" ] == YES )
+            {
+                [ self parseCharactersStartingAtLine:i ];
+            }
+
+        }
+    }
 
     DESTROY(font);
 }
