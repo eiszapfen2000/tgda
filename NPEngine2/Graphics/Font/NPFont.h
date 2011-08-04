feca0d6eed21f0d92cbcc302a26c660c080b2d9e
icicle <icicle@f832cba2-7736-0410-a208-86f5be76f6c5>
2011-08-04 09:50:48 +0000

    TG: NPFontCompiler - parse character pages and characters; NPFont - implement character page and character loading

--- NPEngine2/Graphics/Font/NPFont.h
+++ NPEngine2/Graphics/Font/NPFont.h
@@ -1,7 +1,31 @@
+#import "Core/Math/NpMath.h"
 #import "Core/NPObject/NPObject.h"
 #import "Core/Protocols/NPPPersistentObject.h"
 
 @class NSMutableArray;
+
+typedef struct NpBMFontCharacter
+{
+	int32_t x;
+	int32_t y;
+	int32_t width;
+	int32_t height;
+	int32_t xOffset;
+	int32_t yOffset;
+	int32_t xAdvance;
+	int32_t characterMapIndex;
+}
+NpBMFontCharacter;
+
+typedef struct NpFontCharacter
+{
+	FRectangle source; // coordinates to look up character in texture
+	IVector2 size;
+	IVector2 offset;	
+	int32_t xAdvance;
+	int32_t characterMapIndex;
+}
+NpFontCharacter;
 
 @interface NPFont : NPObject < NPPPersistentObject >
 {
@@ -15,6 +39,7 @@
     int32_t textureWidth;
     int32_t textureHeight;
     NSMutableArray * characterPages;
+    NpFontCharacter * characters;
 }
 
 - (id) init;
@@ -36,6 +61,11 @@
 - (void) setBaseLine:(const int32_t)newBaseLine;
 - (void) setTextureWidth:(const int32_t)newTextureWidth;
 - (void) setTextureHeight:(const int32_t)newTextureHeight;
+
+- (void) addCharacterPageFromFile:(NSString *)fileName;
+- (void) addCharacter:(const NpBMFontCharacter)character
+              atIndex:(const int32_t)index
+                     ;
 
 @end
 
