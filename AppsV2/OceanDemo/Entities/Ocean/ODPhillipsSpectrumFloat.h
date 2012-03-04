a890a691d2d47480eabc073b708eec2db83ba5c1
Thomas Gamper <1222479+eiszapfen2000@users.noreply.github.com>
2012-03-04 09:52:47 +0000

    TG: Add explicit phillips spectrum float implementation

---
+++ AppsV2/OceanDemo/Entities/Ocean/ODPhillipsSpectrumFloat.h
@@ -0,0 +1,22 @@
+#import "fftw3.h"
+#import "Core/NPObject/NPObject.h"
+#import "ODPFrequencySpectrumGeneration.h"
+
+@interface ODPhillipsSpectrumFloat : NPObject
+{
+    fftwf_complex * H0;
+    ODSpectrumSettings lastSettings;
+    ODSpectrumSettings currentSettings;
+}
+
+- (id) init;
+- (id) initWithName:(NSString *)newName;
+- (void) dealloc;
+
+- (void) generateH0;
+- (fftwf_complex *) generateHAtTime:(const float)time;
+- (fftwf_complex *) generateTimeIndependentH;
+- (fftwf_complex *) generateHHCAtTime:(const float)time;
+- (fftwf_complex *) generateTimeIndependentHHC;
+
+@end
