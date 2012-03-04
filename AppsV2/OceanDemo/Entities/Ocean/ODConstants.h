69c6147c2a8876f5a6f26745fc9a65d3718ba64b
Thomas Gamper <1222479+eiszapfen2000@users.noreply.github.com>
2012-03-04 10:29:39 +0000

    TG: ODConstants.h - forgot to add

---
+++ AppsV2/OceanDemo/Entities/Ocean/ODConstants.h
@@ -0,0 +1,17 @@
+#ifndef ODCONSTANTS_H_
+#define ODCONSTANTS_H_
+
+// In meters
+#define EQUATORIAL_RADIUS       6378137
+#define POLAR_RADIUS            6356752.3141
+
+// m/s²
+#define EARTH_ACCELERATION      9.80665
+#define EARTH_ACCELERATIONf     9.80665f
+
+//
+#define PHILLIPS_CONSTANT       0.0081
+#define PHILLIPS_CONSTANTf      0.0081f
+
+
+#endif
