5c1f376c785a893250d067daecf2d046d5736e9c
Thomas Gamper <1222479+eiszapfen2000@users.noreply.github.com>
2012-03-25 17:54:07 +0000

    TG: ODPhillipsSpectrumFloat - performance madness, turns out sinf and cosf are REALLY slow for larger values; ODOceanEntity - dummy code to upload and render generated heightfield; ODPerlinNoise - fix leak; TODO.txt - update

--- AppsV2/OceanDemo/Entities/ODOceanEntity.m
+++ AppsV2/OceanDemo/Entities/ODOceanEntity.m
@@ -1,10 +1,20 @@
 #import <Foundation/NSArray.h>
+#import <Foundation/NSData.h>
 #import <Foundation/NSDictionary.h>
 #import <Foundation/NSException.h>
 #import <Foundation/NSLock.h>
+#import <Foundation/NSPointerArray.h>
 #import <Foundation/NSThread.h>
-#import "Core/Timer/NPTimer.h"
+#import "Core/Container/NPAssetArray.h"
 #import "Core/Thread/NPSemaphore.h"
+#import "Core/Timer/NPTimer.h"
+#import "Graphics/Effect/NPEffect.h"
+#import "Graphics/Effect/NPEffectTechnique.h"
+#import "Graphics/Effect/NPEffectVariableFloat.h"
+#import "Graphics/Texture/NPTexture2D.h"
+#import "Graphics/Texture/NPTextureBindingState.h"
+#import "Graphics/NPOrthographic.h"
+#import "Graphics/NPEngineGraphics.h"
 #import "ODProjector.h"
 #import "ODProjectedGrid.h"
 #import "Ocean/ODPhillipsSpectrum.h"
@@ -14,6 +24,41 @@
 
 static NPSemaphore * semaphore = nil;
 static NSLock * mutex = nil;
+
+void print_complex_spectrum(const IVector2 resolution, fftwf_complex * spectrum)
+{
+    for ( int32_t j = 0; j < resolution.y; j++ )
+    {
+        for ( int32_t k = 0; k < resolution.x; k++ )
+        {
+            printf("%f %fi ", spectrum[k * resolution.y + j][0], spectrum[k * resolution.y + j][1]);
+        }
+
+        printf("\n");
+    }
+}
+
+void print_half_complex_spectrum(const IVector2 resolution, fftwf_complex * spectrum)
+{
+    for ( int32_t j = 0; j < ((resolution.y/2)+1); j++ )
+    {
+        for ( int32_t k = 0; k < resolution.x; k++ )
+        {
+            printf("%f %fi ", spectrum[k * ((resolution.y/2)+1) + j][0], spectrum[k * ((resolution.y/2)+1) + j][1]);
+        }
+
+        printf("\n");
+    }
+}
+
+typedef struct
+{
+    IVector2 resolution;
+    Vector2 size;
+    float  * data32f;
+    double * data64f;
+}
+OdHeightfieldData;
 
 @interface ODOceanEntity (Private)
 
@@ -31,18 +76,10 @@ static NSLock * mutex = nil;
 
     ODSpectrumSettings settings;
     settings.size = (Vector2){10.0f, 10.0f};
-    settings.resolution = (IVector2){8, 8};
-    settings.windDirection = (Vector2){10.0f, 15.0f};
+    settings.resolution = (IVector2){256, 256};
+    settings.windDirection = (Vector2){1.0f, 15.0f};
 
     ODPhillipsSpectrum * s = [[ ODPhillipsSpectrum alloc ] init ];
-
-    fftw_complex * complexHeights
-        = fftw_malloc(sizeof(fftw_complex) * settings.resolution.x * settings.resolution.y);
-
-    fftw_complex * r2c = fftw_malloc(sizeof(fftw_complex) * settings.resolution.x * ((settings.resolution.y / 2) + 1));
-
-    double * c2r = ALLOC_ARRAY(double, settings.resolution.x * settings.resolution.y);
-    double * heights = ALLOC_ARRAY(double, settings.resolution.x * settings.resolution.y);
 
     while ( [[ NSThread currentThread ] isCancelled ] == NO )
     {    
@@ -52,17 +89,26 @@ static NSLock * mutex = nil;
 
         if ( [[ NSThread currentThread ] isCancelled ] == NO )
         {
+            fftwf_complex * complexHeights
+                = fftwf_malloc(sizeof(fftwf_complex) * settings.resolution.x * settings.resolution.y);
+
+            float * c2r = ALLOC_ARRAY(float, settings.resolution.x * settings.resolution.y);
+
+            OdHeightfieldData * result = ALLOC(OdHeightfieldData);
+            result->size = settings.size;
+            result->resolution = settings.resolution;
+
             [ timer update ];
 
-            fftw_complex * complexSpectrum
-                = [ s generateDoubleFrequencySpectrum:settings atTime:1.0];
+            fftwf_complex * complexSpectrum
+                = [ s generateFloatFrequencySpectrum:settings atTime:[ timer totalElapsedTime ]];
 
             [ timer update ];
 
             const float fpsC = [ timer frameTime ];
 
-            fftw_complex * halfcomplexSpectrum
-                = [ s generateDoubleFrequencySpectrumHC:settings atTime:1.0];
+            fftwf_complex * halfcomplexSpectrum
+                = [ s generateFloatFrequencySpectrumHC:settings atTime:[ timer totalElapsedTime ]];
 
             [ timer update ];
 
@@ -71,151 +117,63 @@ static NSLock * mutex = nil;
             printf("PHILLIPS: %f %f\n", fpsC, fpsHC);
             fflush(stdout);
 
+            /*
             printf("spectrum\n");
-
-            for ( int32_t j = 0; j < settings.resolution.y; j++ )
-            {
-                for ( int32_t k = 0; k < settings.resolution.x; k++ )
-                {
-                    printf("%f %fi ", complexSpectrum[k * settings.resolution.y + j][0], complexSpectrum[k * settings.resolution.y + j][1]);
-                }
-
-                printf("\n");
-            }
+            print_complex_spectrum(settings.resolution, complexSpectrum);
             fflush(stdout);
+            */
             
+            /*
             printf("spectrumHC\n");
-
-            for ( int32_t j = 0; j < ((settings.resolution.y/2)+1); j++ )
-            {
-                for ( int32_t k = 0; k < settings.resolution.x; k++ )
-                {
-                    printf("%f %fi ", halfcomplexSpectrum[k * ((settings.resolution.y/2)+1) + j][0], halfcomplexSpectrum[k * ((settings.resolution.y/2)+1) + j][1]);
-                }
-
-                printf("\n");
-            }
+            print_half_complex_spectrum(settings.resolution, halfcomplexSpectrum);
             fflush(stdout);
-
+            */
 
             [ timer update ];
-            fftw_plan plan;
-            plan = fftw_plan_dft_2d(settings.resolution.x,
+            fftwf_plan plan;
+            plan = fftwf_plan_dft_2d(settings.resolution.x,
                                     settings.resolution.y,
                                     complexSpectrum,
                                     complexHeights,
                                     FFTW_BACKWARD,
                                     FFTW_ESTIMATE);
 
-            fftw_execute(plan);
-            fftw_destroy_plan(plan);
+            fftwf_execute(plan);
+            fftwf_destroy_plan(plan);
             [ timer update ];
 
             const float fpsIFFTC = [ timer frameTime ];
 
-            // write real part to result array
-            for ( int32_t j = 0; j < settings.resolution.x; j++ )
-            {
-                for ( int32_t k = 0; k < settings.resolution.y; k++ )
-                {
-                    heights[k + settings.resolution.y * j] = complexHeights[k + settings.resolution.y * j][0];
-                }
-            }
-
-            /*
-            printf("heights\n");
-            for ( int32_t j = 0; j < spectrumResolution.y; j++ )
-            {
-                for ( int32_t k = 0; k < spectrumResolution.x; k++ )
-                {
-                    printf("%f ", heights[k * spectrumResolution.y + j]);
-                }
-
-                printf("\n");
-            }
-            fflush(stdout);
-            */
-
-            /*
-            plan = fftw_plan_dft_r2c_2d(spectrumResolution.x,
-                                        spectrumResolution.y,
-                                        heights,
-                                        r2c,
-                                        FFTW_ESTIMATE);
-
-            fftw_execute(plan);
-            fftw_destroy_plan(plan);
-            */
-
-            /*
-            printf("r2c spectrum\n");
-            for ( int32_t j = 0; j < ((spectrumResolution.y/2)+1); j++ )
-            {
-                for ( int32_t k = 0; k < spectrumResolution.x; k++ )
-                {
-                    printf("%f %fi ", r2c[k * ((spectrumResolution.y/2)+1) + j][0], r2c[k * ((spectrumResolution.y/2)+1) + j][1]);
-                }
-
-                printf("\n");
-            }
-            fflush(stdout);
-            */
-
             [ timer update ];
-            plan = fftw_plan_dft_c2r_2d(settings.resolution.x,
+            plan = fftwf_plan_dft_c2r_2d(settings.resolution.x,
                                         settings.resolution.y,
-                                        //r2c,
                                         halfcomplexSpectrum,
                                         c2r,
                                         FFTW_ESTIMATE);
 
-            fftw_execute(plan);
-            fftw_destroy_plan(plan);
+            fftwf_execute(plan);
+            fftwf_destroy_plan(plan);
             [ timer update ];
 
             const float fpsIFFTHC = [ timer frameTime ];
 
-            /*
-            printf("heights HC\n");
-            for ( int32_t j = 0; j < spectrumResolution.y; j++ )
-            {
-                for ( int32_t k = 0; k < spectrumResolution.x; k++ )
-                {
-                    printf("%f ", c2r[k * spectrumResolution.y + j]);
-                }
+            //printf("IFFT: %f %f\n", fpsIFFTC, fpsIFFTHC);
+            //fflush(stdout);
 
-                printf("\n");
-            }
-            fflush(stdout);
-            */
+            fftwf_free(complexSpectrum);
+            fftwf_free(halfcomplexSpectrum);
 
-            printf("IFFT: %f %f\n", fpsIFFTC, fpsIFFTHC);
-            fflush(stdout);
+            fftwf_free(complexHeights);
+            //SAFE_FREE(c2r);
 
-            /*
-            printf("c2r spectrum\n");
-            for ( int32_t j = 0; j < spectrumResolution.y; j++ )
-            {
-                for ( int32_t k = 0; k < spectrumResolution.x; k++ )
-                {
-                    printf("%f ", c2r[k * spectrumResolution.y + j] / (spectrumResolution.x * spectrumResolution.y));
-                }
-                printf("\n");
-            }
-            fflush(stdout);
-            */
-
-            fftw_free(complexSpectrum);
-            fftw_free(halfcomplexSpectrum);
+            result->data32f = c2r;
+            [ mutex lock ];
+            [ resultQueue addPointer:result ];
+            [ mutex unlock ];
         }
 
         DESTROY(innerPool);
     }    
-
-    fftw_free(complexHeights);
-    fftw_free(r2c);
-    SAFE_FREE(heights);
-    SAFE_FREE(c2r);
 
     DESTROY(s);
     DESTROY(timer);
@@ -235,9 +193,21 @@ static NSLock * mutex = nil;
 {
     self =  [ super initWithName:newName ];
 
+    NSPointerFunctionsOptions options
+        = NSPointerFunctionsOpaqueMemory | NSPointerFunctionsOpaquePersonality;
+    resultQueue = [[ NSPointerArray alloc ] initWithOptions:options ];
+
     projector = [[ ODProjector alloc ] initWithName:@"Projector" ];
     projectedGrid = [[ ODProjectedGrid alloc ] initWithName:@"Projected Grid" ];
     [ projectedGrid setProjector:projector ];
+
+    heightfield = [[ NPTexture2D alloc ] initWithName:@"Height Texture" ];
+
+    effect
+        = [[[ NPEngineGraphics instance ] effects ]
+                getAssetWithFileName:@"fullscreen.effect" ];
+
+    ASSERT_RETAIN(effect);
 
     return self;
 }
@@ -245,9 +215,11 @@ static NSLock * mutex = nil;
 - (void) dealloc
 {
     SAFE_DESTROY(stateset);
-
+    DESTROY(effect);
+    DESTROY(heightfield);
     DESTROY(projector);
     DESTROY(projectedGrid);
+    DESTROY(resultQueue);
 
     [ super dealloc ];
 }
@@ -364,7 +336,56 @@ static NSLock * mutex = nil;
 
 - (void) render
 {
+    OdHeightfieldData * hf = NULL;
+
+    [ mutex lock ];
+    if ( [ resultQueue count ] != 0)
+    {
+        //NSLog(@"%u", (uint32_t)[ resultQueue count ]);
+        hf = [ resultQueue pointerAtIndex:0 ];
+        [ resultQueue removePointerAtIndex:0 ];
+    }
+    [ mutex unlock ];
+
+    if ( hf != NULL )
+    {
+        const NSUInteger numberOfBytes
+            = hf->resolution.x * hf->resolution.y * sizeof(float);
+
+        NSData * textureData
+            = [ NSData dataWithBytesNoCopy:hf->data32f
+                                    length:numberOfBytes
+                              freeWhenDone:NO ];
+
+        [ heightfield generateUsingWidth:hf->resolution.x
+                                  height:hf->resolution.y
+                             pixelFormat:NpImagePixelFormatR
+                              dataFormat:NpImageDataFormatFloat32
+                                 mipmaps:NO
+                                    data:textureData ];
+
+        SAFE_FREE(hf->data32f);
+    }
+
     [ projectedGrid render ];
+
+    [[[ NPEngineGraphics instance ] orthographic ] activate ];
+    [[[ NPEngineGraphics instance ] textureBindingState ] setTexture:heightfield texelUnit:0 ];
+    [[[ NPEngineGraphics instance ] textureBindingState ] activate ];
+    [[ effect techniqueWithName:@"texture" ] activate ];
+    glBegin(GL_QUADS);
+        glVertexAttrib2f(NpVertexStreamTexCoords0, 0.0f, 0.0f);
+        glVertex2i(0, 0);
+        glVertexAttrib2f(NpVertexStreamTexCoords0, 1.0f, 0.0f);
+        glVertex2i(1, 0);
+        glVertexAttrib2f(NpVertexStreamTexCoords0, 1.0f, 1.0f);
+        glVertex2i(1, 1);
+        glVertexAttrib2f(NpVertexStreamTexCoords0, 0.0f, 1.0f);
+        glVertex2i(0, 1);
+    glEnd();
+
+    [[[ NPEngineGraphics instance ] orthographic ] deactivate ];
+
     [ projector     render ];
 }
 
