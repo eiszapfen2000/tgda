fb2f391530647a104fae6bbb7cc2c9e696e91749
icicle <icicle@f832cba2-7736-0410-a208-86f5be76f6c5>
2012-04-20 14:31:58 +0000

    TG: NpCrc32 - fix useless type conversions; NpHalf - fix border cases to return correct values

--- NPEngine2/Core/Basics/NpHalf.c
+++ NPEngine2/Core/Basics/NpHalf.c
@@ -9,9 +9,11 @@
 
 float half_to_float(Half h)
 {
-    int s = (h >> 15) & 0x00000001;
-    int e = (h >> 10) & 0x0000001f;
-    int m =  h        & 0x000003ff;
+    float fResult;
+
+    uint32_t s = (h >> 15) & 0x00000001;
+    uint32_t e = (h >> 10) & 0x0000001f;
+    uint32_t m =  h        & 0x000003ff;
 
     if (e == 0)
     {
@@ -20,8 +22,10 @@ float half_to_float(Half h)
 	        //
 	        // Plus or minus zero
 	        //
+            uint32_t result = s << 31;
 
-	        return s << 31;
+            memcpy(&fResult, &result, sizeof(float));
+	        return fResult;
 	    }
 	    else
 	    {
@@ -29,14 +33,14 @@ float half_to_float(Half h)
 	        // Denormalized number -- renormalize it
 	        //
 
-	        while (!(m & 0x00000400))
+	        while (!(m & 0x00000400u))
 	        {
 		        m <<= 1;
 		        e -=  1;
 	        }
 
 	        e += 1;
-	        m &= ~0x00000400;
+	        m &= ~0x00000400u;
 	    }
     }
     else if (e == 31)
@@ -47,7 +51,10 @@ float half_to_float(Half h)
 	        // Positive or negative infinity
 	        //
 
-	        return (s << 31) | 0x7f800000;
+            uint32_t result = (s << 31) | 0x7f800000u;
+
+            memcpy(&fResult, &result, sizeof(float));
+	        return fResult;
 	    }
 	    else
 	    {
@@ -55,7 +62,10 @@ float half_to_float(Half h)
 	        // Nan -- preserve sign and significand bits
 	        //
 
-	        return (s << 31) | 0x7f800000 | (m << 13);
+            uint32_t result = (s << 31) | 0x7f800000u | (m << 13);
+
+            memcpy(&fResult, &result, sizeof(float));
+	        return fResult;
 	    }
     }
 
@@ -69,7 +79,6 @@ float half_to_float(Half h)
     //
     // Assemble s, e and m.
     //
-    float fResult;
     uint32_t result = (s << 31) | (e << 23) | m;
 
     // Avoid strict aliasing issues
