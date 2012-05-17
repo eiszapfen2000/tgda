72be6299b1dd2256847bb5b5465a99f91f60407a
Thomas Gamper <1222479+eiszapfen2000@users.noreply.github.com>
2012-05-17 09:37:58 +0000

    TG: NPPRenderTarget - add common protocol for render buffers and render textures; NPRenderBuffer - adhere to NPPRenderTarget, support color targets; NPRenderTexture - adhere to NPPRenderTarget, support depth and depthstencil targets; NPRenderTargetConfiguration - adjust types to id <NPPRenderTarget>

---
+++ NPEngine2/Graphics/RenderTarget/NPPRenderTarget.h
@@ -0,0 +1,19 @@
+#import "Core/NPObject/NPObject.h"
+#import "GL/glew.h"
+
+@class NPRenderTargetConfiguration;
+
+@protocol NPPRenderTarget < NSObject >
+
+- (GLuint) glID;
+- (uint32_t) width;
+- (uint32_t) height;
+
+- (void) attachToRenderTargetConfiguration:(NPRenderTargetConfiguration *)configuration
+                          colorBufferIndex:(uint32_t)newColorBufferIndex
+                                   bindFBO:(BOOL)bindFBO
+                                          ;
+
+- (void) detach:(BOOL)bindFBO;
+
+@end
--- NPEngine2/Graphics/RenderTarget/NPRenderTexture.h
+++ NPEngine2/Graphics/RenderTarget/NPRenderTexture.h
@@ -1,32 +1,30 @@
 #import "GL/glew.h"
 #import "Core/NPObject/NPObject.h"
+#import "NPPRenderTarget.h"
 #import "Graphics/NPEngineGraphicsEnums.h"
 
 @class NSError;
 @class NPRenderTargetConfiguration;
 @class NPTexture2D;
 
-@interface NPRenderTexture : NPObject
+@interface NPRenderTexture : NPObject < NPPRenderTarget >
 {
 	GLuint glID;
     NpRenderTargetType type;
 	uint32_t width;
 	uint32_t height;
-    NpImagePixelFormat pixelFormat;
-    NpRenderBufferDataFormat dataFormat;
+    NpTexturePixelFormat pixelFormat;
+    NpTextureDataFormat dataFormat;
     NPTexture2D * texture;
-    BOOL ready;
     NPRenderTargetConfiguration * rtc;
     uint32_t colorBufferIndex;
+    BOOL ready;
 }
 
 - (id) init;
 - (id) initWithName:(NSString *)newName;
 - (void) dealloc;
 
-- (GLuint) glID;
-- (uint32_t) width;
-- (uint32_t) height;
 - (NPTexture2D *) texture;
 
 - (BOOL) generate:(NpRenderTargetType)newType
@@ -37,12 +35,6 @@
     mipmapStorage:(BOOL)mipmapStorage
             error:(NSError **)error
                  ;
-
-- (void) attachToRenderTargetConfiguration:(NPRenderTargetConfiguration *)configuration
-                          colorBufferIndex:(uint32_t)newColorBufferIndex
-                                   bindFBO:(BOOL)bindFBO
-                                          ;
-- (void) detach:(BOOL)bindFBO;
 
 @end
 
