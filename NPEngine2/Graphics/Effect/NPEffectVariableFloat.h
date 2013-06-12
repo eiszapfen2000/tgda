be1b4391e9716b46fb4d3a29876f9fc5c588ce24
icicle <icicle@f832cba2-7736-0410-a208-86f5be76f6c5>
2013-06-12 13:41:12 +0000

    TG: NPEffectVariableFloat - add classes for 2x2, 3x3 and 4x4 matrix uniforms; NPEngineGraphicsStringToClassConversion - add appropriate mapping for new uniform type classes

--- NPEngine2/Graphics/Effect/NPEffectVariableFloat.h
+++ NPEngine2/Graphics/Effect/NPEffectVariableFloat.h
@@ -1,6 +1,8 @@
 #import "Core/Basics/NpTypes.h"
 #import "Core/Math/FVector.h"
 #import "Core/Math/Vector.h"
+#import "Core/Math/FMatrix.h"
+#import "Core/Math/Matrix.h"
 #import "NPEffectVariableUniform.h"
 
 @class NPEffectTechniqueVariable;
@@ -64,6 +66,54 @@
 - (FVector4) value;
 - (void) setFValue:(FVector4)newValue;
 - (void) setValue:(Vector4)newValue;
+
+- (void) activate:(NPEffectTechniqueVariable *)variable;
+
+@end
+
+@interface NPEffectVariableMatrix2x2 : NPEffectVariableUniform
+{
+    FMatrix2 value;
+}
+
+- (id) init;
+- (id) initWithName:(NSString *)newName;
+
+- (const FMatrix2 *) value;
+- (void) setFValue:(const FMatrix2 * const)newValue;
+- (void) setValue:(const Matrix2 * const)newValue;
+
+- (void) activate:(NPEffectTechniqueVariable *)variable;
+
+@end
+
+@interface NPEffectVariableMatrix3x3 : NPEffectVariableUniform
+{
+    FMatrix3 value;
+}
+
+- (id) init;
+- (id) initWithName:(NSString *)newName;
+
+- (const FMatrix3 *) value;
+- (void) setFValue:(const FMatrix3 * const)newValue;
+- (void) setValue:(const Matrix3 * const)newValue;
+
+- (void) activate:(NPEffectTechniqueVariable *)variable;
+
+@end
+
+@interface NPEffectVariableMatrix4x4 : NPEffectVariableUniform
+{
+    FMatrix4 value;
+}
+
+- (id) init;
+- (id) initWithName:(NSString *)newName;
+
+- (const FMatrix4 *) value;
+- (void) setFValue:(const FMatrix4 * const)newValue;
+- (void) setValue:(const Matrix4 * const)newValue;
 
 - (void) activate:(NPEffectTechniqueVariable *)variable;
 
--- NPEngine2/Graphics/Effect/NPEffectVariableFloat.m
+++ NPEngine2/Graphics/Effect/NPEffectVariableFloat.m
@@ -163,3 +163,120 @@
 
 @end
 
+@implementation NPEffectVariableMatrix2x2
+
+- (id) init
+{
+    [ self notImplemented:_cmd ];
+    return nil;
+}
+
+- (id) initWithName:(NSString *)newName
+{
+    self = [ super initWithName:newName uniformType:NpUniformFloat4 ];
+
+    fm2_m_set_identity(&value);
+
+    return self;
+}
+
+- (const FMatrix2 *) value;
+{
+    return &value;
+}
+
+- (void) setFValue:(const FMatrix2 * const)newValue
+{
+    value = *newValue;
+}
+
+- (void) setValue:(const Matrix2 * const)newValue
+{
+    fm2_m_init_with_m2(&value, newValue);
+}
+
+- (void) activate:(NPEffectTechniqueVariable *)variable
+{
+    glUniformMatrix2fv([ variable location ], 1, GL_FALSE, (const GLfloat *)(value.elements));
+}
+
+@end
+
+@implementation NPEffectVariableMatrix3x3
+
+- (id) init
+{
+    [ self notImplemented:_cmd ];
+    return nil;
+}
+
+- (id) initWithName:(NSString *)newName
+{
+    self = [ super initWithName:newName uniformType:NpUniformFloat4 ];
+
+    fm3_m_set_identity(&value);
+
+    return self;
+}
+
+- (const FMatrix3 *) value;
+{
+    return &value;
+}
+
+- (void) setFValue:(const FMatrix3 * const)newValue
+{
+    value = *newValue;
+}
+
+- (void) setValue:(const Matrix3 * const)newValue
+{
+    fm3_m_init_with_m3(&value, newValue);
+}
+
+- (void) activate:(NPEffectTechniqueVariable *)variable
+{
+    glUniformMatrix3fv([ variable location ], 1, GL_FALSE, (const GLfloat *)(value.elements));
+}
+
+@end
+
+@implementation NPEffectVariableMatrix4x4
+
+- (id) init
+{
+    [ self notImplemented:_cmd ];
+    return nil;
+}
+
+- (id) initWithName:(NSString *)newName
+{
+    self = [ super initWithName:newName uniformType:NpUniformFloat4 ];
+
+    fm4_m_set_identity(&value);
+
+    return self;
+}
+
+- (const FMatrix4 *) value;
+{
+    return &value;
+}
+
+- (void) setFValue:(const FMatrix4 * const)newValue
+{
+    value = *newValue;
+}
+
+- (void) setValue:(const Matrix4 * const)newValue
+{
+    fm4_m_init_with_m4(&value, newValue);
+}
+
+- (void) activate:(NPEffectTechniqueVariable *)variable
+{
+    glUniformMatrix4fv([ variable location ], 1, GL_FALSE, (const GLfloat *)(value.elements));
+}
+
+@end
+
