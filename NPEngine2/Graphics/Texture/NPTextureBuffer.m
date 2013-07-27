a5acff489515b5f52b9265f5e8debc02f808c1e0
icicle <icicle@f832cba2-7736-0410-a208-86f5be76f6c5>
2013-07-27 10:13:09 +0000

    TG: NPTextureBuffer - implement buffer attachment and detachment

--- NPEngine2/Graphics/Texture/NPTextureBuffer.m
+++ NPEngine2/Graphics/Texture/NPTextureBuffer.m
@@ -1,3 +1,6 @@
+#import <Foundation/NSException.h>
+#import "Graphics/NPEngineGraphics.h"
+#import "NPTextureBindingState.h"
 #import "NPTextureBuffer.h"
 
 @implementation NPTextureBuffer
@@ -12,12 +15,15 @@
     self = [ super initWithName:newName ];
 
     glGenTextures(1, &textureID);
+    buffer = nil;
 
     return self;
 }
 
 - (void) dealloc
 {
+    SAFE_DESTROY(buffer);
+
     if (textureID > 0 )
     {
         glDeleteTextures(1, &textureID);
@@ -40,6 +46,36 @@
 - (GLenum) glTarget
 {
     return GL_TEXTURE_BUFFER;
+}
+
+- (void) attachBuffer:(NPBufferObject *)newBuffer
+     numberOfElements:(NSUInteger)newNumberOfElements
+          pixelFormat:(NpTexturePixelFormat)newPixelFormat
+           dataFormat:(NpTextureDataFormat)newDataFormat
+{
+    NSAssert(newBuffer != nil, @"");
+
+    GLint glInternalFormat
+        = getGLTextureInternalFormat(newDataFormat, newPixelFormat, YES,
+                                     NULL, NULL);
+
+    [[[ NPEngineGraphics instance ] textureBindingState ] setTextureImmediately:self ];
+    glTexBuffer(GL_TEXTURE_BUFFER, glInternalFormat, [ buffer glID ]);
+    [[[ NPEngineGraphics instance ] textureBindingState ] restoreOriginalTextureImmediately ];
+
+    buffer = RETAIN(newBuffer);
+}
+
+- (void) detachBuffer
+{
+    if ( buffer != nil )
+    {
+        [[[ NPEngineGraphics instance ] textureBindingState ] setTextureImmediately:self ];
+        glTexBuffer(GL_TEXTURE_BUFFER, GL_NONE, 0);
+        [[[ NPEngineGraphics instance ] textureBindingState ] restoreOriginalTextureImmediately ];
+
+        DESTROY(buffer);
+    }
 }
 
 @end
