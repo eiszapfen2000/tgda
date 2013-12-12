ef787dab1c05e4085590b45e6fdf5dd349dfca4b
Thomas Gamper <1222479+eiszapfen2000@users.noreply.github.com>
2013-12-12 12:27:27 +0000

    TG: - freq_spec_disp.m compute partial derivatives of displacement

--- Matlab/freq_spec_disp.m
+++ Matlab/freq_spec_disp.m
@@ -13,10 +13,8 @@ ky2tmp = realpow(k(:,:,2), 2);
 kx2y2tmp = kx2tmp + ky2tmp;
 knorm = realsqrt(kx2y2tmp);
 
-%dxTerm = -1i .* k(:,:,1) ./ knorm;
-%dyTerm = -1i .* k(:,:,2) ./ knorm;
-dxTerm = 1i .* k(:,:,1) ./ knorm;
-dyTerm = 1i .* k(:,:,2) ./ knorm;
+dxTerm = -1i .* k(:,:,1) ./ knorm;
+dyTerm = -1i .* k(:,:,2) ./ knorm;
 dxTerm(isnan(dxTerm))=0;
 dyTerm(isnan(dyTerm))=0;
 
@@ -25,5 +23,13 @@ dispy = dyTerm .* freqspectrum;
 
 dispx(:,1) = 0;
 dispy(1,:) = 0;
+
+dispx_x = 1i .* k(:,:,1) .* dispx;
+dispy_y = 1i .* k(:,:,2) .* dispy;
+dispx_y = 1i .* k(:,:,2) .* dispx;
+dispy_x = 1i .* k(:,:,1) .* dispy;
+
+dispy_x(:,1) = 0;
+dispx_y(1,:) = 0;
 
 end
