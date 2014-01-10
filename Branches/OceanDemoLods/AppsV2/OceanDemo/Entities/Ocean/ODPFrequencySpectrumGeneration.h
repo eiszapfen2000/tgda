6336860fbb0af8012a6c9e9bd7b09b22f852b148
icicle <icicle@f832cba2-7736-0410-a208-86f5be76f6c5>
2014-01-10 19:02:54 +0000

    TG: ODPFrequencySpectrumGeneration - split complex and halfcomplex data into different structs; fix fallout

--- Branches/OceanDemoLods/AppsV2/OceanDemo/Entities/Ocean/ODPFrequencySpectrumGeneration.h
+++ Branches/OceanDemoLods/AppsV2/OceanDemo/Entities/Ocean/ODPFrequencySpectrumGeneration.h
@@ -56,24 +56,49 @@ bool phillips_settings_equal(const ODPhillipsGeneratorSettings * pOne, const ODP
 bool unified_settings_equal(const ODUnifiedGeneratorSettings * pOne, const ODUnifiedGeneratorSettings * pTwo);
 bool generator_settings_equal(const ODGeneratorSettings * pOne, const ODGeneratorSettings * pTwo);
 
+typedef struct OdSpectrumDataFloat
+{
+    // zero frequency upper left
+    fftwf_complex * height;
+    fftwf_complex * gradient;
+    fftwf_complex * displacement;
+    fftwf_complex * displacementXdXdZ;
+    fftwf_complex * displacementZdXdZ;
+}
+OdSpectrumDataFloat;
+
+typedef struct OdSpectrumDataHCFloat
+{
+    // zero frequency upper left
+    fftwf_complex * height;
+    fftwf_complex * gradientX;
+    fftwf_complex * gradientZ;
+    fftwf_complex * displacementX;
+    fftwf_complex * displacementZ;
+    fftwf_complex * displacementXdX;
+    fftwf_complex * displacementXdZ;
+    fftwf_complex * displacementZdX;
+    fftwf_complex * displacementZdZ;
+}
+OdSpectrumDataHCFloat;
+
 typedef struct OdFrequencySpectrumFloat
 {
     float timestamp;
     IVector2 geometryResolution;
     IVector2 gradientResolution;
-    Vector2 size;
-    float * baseSpectrum; // zero frequency at center
+    Vector2  size;
+
+    // zero frequency at center
+    float * baseSpectrum;
     float maxMeanSlopeVariance;
     float effectiveMeanSlopeVariance;
-    fftwf_complex * height; // zero frequency upper left
-    fftwf_complex * gradientX;
-    fftwf_complex * gradientZ;
-    fftwf_complex * gradient;
-    fftwf_complex * displacementX;
-    fftwf_complex * displacementZ;
-    fftwf_complex * displacement;
-    fftwf_complex * displacementXdXdZ;
-    fftwf_complex * displacementZdXdZ;
+
+    union
+    {
+        OdSpectrumDataFloat   data;
+        OdSpectrumDataHCFloat dataHC;
+    };
 }
 OdFrequencySpectrumFloat;
 
