6fec9ccdcb8b876bdd98a6ee42ce2be3a7935f87
icicle <icicle@f832cba2-7736-0410-a208-86f5be76f6c5>
2014-01-23 18:55:38 +0000

    TG: FTriangle - add is_point_inside function

--- Branches/OceanDemoLods/NPEngine2/Core/Math/FTriangle.c
+++ Branches/OceanDemoLods/NPEngine2/Core/Math/FTriangle.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include "Core/Basics/NpFreeList.h"
 #include "FTriangle.h"
 
@@ -37,4 +38,24 @@ void ftriangle_tvvv_init_with_vertices(FTriangle * triangle,
     triangle->a = *a;
     triangle->b = *b;
     triangle->c = *c;
+}
+
+int32_t ftriangle_vt_is_point_inside(const FVector2 * const point,
+    const FTriangle * const triangle)
+{
+    const FVector2 ab = fv2_vv_sub(&triangle->b, &triangle->a);
+    const FVector2 bc = fv2_vv_sub(&triangle->c, &triangle->b);
+    const FVector2 ca = fv2_vv_sub(&triangle->a, &triangle->c);
+
+    const FVector2 ap = fv2_vv_sub(point, &triangle->a);
+    const FVector2 bp = fv2_vv_sub(point, &triangle->b);
+    const FVector2 cp = fv2_vv_sub(point, &triangle->c);
+
+    // ABxAP, BCxBP and CAxCP
+    // (x1,y1,0)x(x2,y2,0)=(0,0,x1y2−x2y1)
+    const float signOne   = copysignf(1.0f, ab.x * ap.y - ab.y * ap.x);
+    const float signTwo   = copysignf(1.0f, bc.x * bp.y - bc.y * bp.x);
+    const float signThree = copysignf(1.0f, ca.x * cp.y - ca.y * cp.x);
+
+    return (signOne == signTwo && signOne == signThree) ? 1 : 0;
 }
