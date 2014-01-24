5e740d01ff699591bb1a18482dca875971cfe434
icicle <icicle@f832cba2-7736-0410-a208-86f5be76f6c5>
2014-01-24 10:26:22 +0000

    TG: ODStepItem - some parsing

--- Branches/OceanDemoLods/AppsV2/OceanDemo/Menu/ODStepItem.h
+++ Branches/OceanDemoLods/AppsV2/OceanDemo/Menu/ODStepItem.h
@@ -3,10 +3,18 @@
 @class NPEffectTechnique;
 @class NPEffectVariableFloat4;
 
+enum
+{
+    OdStepItemUnknownMode = -1,
+    OdStepItemIntegerMode =  0,
+    OdStepItemFloatMode   =  1
+};
+
 @interface ODStepItem : ODMenuItem
 {
     NSString * label;
 
+    NSInteger mode;
     NSInteger minimumIntegerValue;
     NSInteger maximumIntegerValue;
     NSInteger integerStep;
