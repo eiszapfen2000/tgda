8cd7b957e7ec333bf7e1040f06595338f1eb4ab1
icicle <icicle@f832cba2-7736-0410-a208-86f5be76f6c5>
2014-01-29 14:48:01 +0000

    TG: ODPreethamSkylight - add basics for sun disc computation

--- Branches/OceanDemoLods/AppsV2/OceanDemo/Entities/ODPreethamSkylight.m
+++ Branches/OceanDemoLods/AppsV2/OceanDemo/Entities/ODPreethamSkylight.m
@@ -18,6 +18,67 @@
 #import "NP.h"
 #import "ODPreethamSkylight.h"
 
+/*
+sun disc implementation from
+
+http://svn.gna.org/svn/pflex-framework/trunk/examples/flexray/lights/distant/preetham/preetham.cpp
+
+*/
+
+#define NUMBER_OF_SPECTRAL_COMPONENTS   41
+
+static const double sun_spectral_radiance[NUMBER_OF_SPECTRAL_COMPONENTS] =
+{
+//in W.cm^{-2}.um^{-1}.sr^{-1}
+
+1655.9,  1623.37, 2112.75, 2588.82, 2582.91, 2423.23, 2676.05, 2965.83, 3054.54, 3005.75,
+3066.37, 2883.04, 2871.21, 2782.5,  2710.06, 2723.36, 2636.13, 2550.38, 2506.02, 2531.16,
+2535.59, 2513.42, 2463.15, 2417.32, 2368.53, 2321.21, 2282.77, 2233.98, 2197.02, 2152.67,
+2109.79, 2072.83, 2024.04, 1987.08, 1942.72, 1907.24, 1862.89, 1825.92,     0.0,     0.0,
+0.0
+};
+
+static const double sun_spectral_k_o[NUMBER_OF_SPECTRAL_COMPONENTS] =
+{
+0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.003, 0.006, 0.009,
+0.014, 0.021, 0.03, 0.04, 0.048, 0.063, 0.075, 0.085, 0.103, 0.12,
+0.12, 0.115, 0.125, 0.12, 0.105, 0.09, 0.079, 0.067, 0.057, 0.048,
+0.036, 0.028, 0.023, 0.018, 0.014, 0.011, 0.01, 0.009, 0.007, 0.004,
+0.0
+};
+
+static const double sun_spectral_k_wa[NUMBER_OF_SPECTRAL_COMPONENTS] =
+{
+0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+0.0, 0.016, 0.024, 0.0125, 1.0, 0.87, 0.061, 0.001, 1e-05, 1e-05,
+0.0006
+};
+
+static const double sun_spectral_k_g[NUMBER_OF_SPECTRAL_COMPONENTS] =
+{
+0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 3.0, 0.21,
+0.0
+};
+
+static const double lambda_micrometer[NUMBER_OF_SPECTRAL_COMPONENTS] =
+{
+0.380, 0.390, 0.400, 0.410, 0.420, 0.430, 0.440, 0.450, 0.460, 0.470,
+0.480, 0.490, 0.500, 0.510, 0.520, 0.530, 0.540, 0.550, 0.560, 0.570,
+0.580, 0.590, 0.600, 0.610, 0.620, 0.630, 0.640, 0.650, 0.660, 0.670,
+0.680, 0.690, 0.700, 0.710, 0.720, 0.730, 0.740, 0.750, 0.760, 0.770,
+0.780
+};
+
+static const double xyz_matching_functions[3][NUMBER_OF_SPECTRAL_COMPONENTS] =
+{
+{0.000159952,0.0023616,0.0191097,0.084736,0.204492,0.314679,0.383734,0.370702,0.302273,0.195618,0.080507,0.016172,0.003816,0.037465,0.117749,0.236491,0.376772,0.529826,0.705224,0.878655,1.01416,1.11852,1.12399,1.03048,0.856297,0.647467,0.431567,0.268329,0.152568,0.0812606,0.0408508,0.0199413,0.00957688,0.00455263,0.00217496,0.00104476,0.000508258,0.000250969,0.00012639,6.45258E-05,3.34117E-05},																   {1.7364e-05,0.0002534,0.0020044,0.008756,0.021391,0.038676,0.062077,0.089456,0.128201,0.18519,0.253589,0.339133,0.460777,0.606741,0.761757,0.875211,0.961988,0.991761,0.99734,0.955552,0.868934,0.777405,0.658341,0.527963,0.398057,0.283493,0.179828,0.107633,0.060281,0.0318004,0.0159051,0.0077488,0.00371774,0.00176847,0.00084619,0.00040741,0.00019873,9.8428e-05,4.9737e-05,2.5486e-05,1.3249e-05},																		   {0.000704776,0.0104822,0.0860109,0.389366,0.972542,1.55348,1.96728,1.9948,1.74537,1.31756,0.772125,0.415254,0.218502,0.112044,0.060709,0.030451,0.013676,0.003988,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+};
+
 static Vector3 preetham_zenith_color(double turbidity, double thetaSun)
 {
     // zenith color computation
@@ -68,6 +129,71 @@ static double digamma(double theta, double gamma, double ABCDE[5])
     const double term_two = 1.0 + ABCDE[2] * exp(ABCDE[3] * gamma) + (ABCDE[4] * cosGamma * cosGamma);
 
     return term_one * term_two;
+}
+
+static Vector3 sun_color(double turbidity, double thetaSun)
+{
+    // transmittance computation
+    // A Practical Analytic Model for Daylight                              
+    // page 21 
+
+    const double thetaSunDegrees = MATH_RAD_TO_DEG * thetaSun;
+    const double m = 1.0 / (thetaSunDegrees + 0.15 * pow(93.885 - thetaSunDegrees, -1.253));
+
+    const double beta = 0.04608 * turbidity - 0.04586;
+    const double l = 0.35;
+    const double alpha = 1.3;
+    const double w = 2.0;
+
+    double spectralRadiance[NUMBER_OF_SPECTRAL_COMPONENTS];
+    memset(spectralRadiance, 0, sizeof(spectralRadiance));
+
+    for ( int32_t i = 0; i < NUMBER_OF_SPECTRAL_COMPONENTS; i++ )
+    {
+        const double lambda = lambda_micrometer[i];
+
+		//apply each transmittance function, no particular order.
+        double exponent = 0.0;
+
+		//Rayleigh
+		exponent += -0.008735 * pow(lambda, -4.08 * m);
+
+		//Angstrom
+		exponent += -beta * powf (lambda, - alpha * m);
+
+		//ozone
+		exponent += -sun_spectral_k_o[i] * l * m;
+
+		//mixed gases absorption
+		const double k_g = sun_spectral_k_g[i];
+		exponent += (-1.41 * k_g * m) / pow(1.0 + 118.93 * k_g * m, 0.45);
+
+		//water vapor absorption
+		const double k_wa = sun_spectral_k_wa[i];
+		exponent += (-0.2385 * k_wa * w * m) / pow(1.0 + 20.07 * k_wa * w * m, 0.45);
+
+		spectralRadiance[i] = sun_spectral_radiance[i] * exp(exponent);
+    }
+
+	//the sun spectral radiances are expressed in cm^{-2} => we have to scale it by 10000 to obtain
+	//the spectral radiance in W.m^{-2}.um^{-1}.sr^{-1},
+	
+	//the sun spectral radiances have wavelengthes expressed in micro-meters => we first have to convert it to wavelengthes
+	//expressed in nanometers, as the color matching functions are expressed with wavelengthes in nanometers => we scale the
+	//spectral radiance by 0.001. The delta_lambda is 10nm => the scaling factor for the wavelength change of unit is 0.01f	
+
+    Vector3 XYZ = v3_zero();
+	
+	float delta = 10000.f * 0.01f;	
+
+	for ( int32_t i = 0; i < NUMBER_OF_SPECTRAL_COMPONENTS; i++ )
+    {
+        XYZ.x += spectralRadiance[i] * xyz_matching_functions[0][i] * delta;
+        XYZ.y += spectralRadiance[i] * xyz_matching_functions[1][i] * delta;
+        XYZ.z += spectralRadiance[i] * xyz_matching_functions[2][i] * delta;
+    }
+
+    return XYZ;
 }
 
 @interface ODPreethamSkylight (Private)
