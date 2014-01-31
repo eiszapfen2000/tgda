1c8926cbadea5763edf171621196104d222e072a
icicle <icicle@f832cba2-7736-0410-a208-86f5be76f6c5>
2014-01-31 16:14:53 +0000

    TG: ODProjector - implement Claes Johanson method from his master's thesis

--- Branches/OceanDemoLods/AppsV2/OceanDemo/Entities/ODProjector.m
+++ Branches/OceanDemoLods/AppsV2/OceanDemo/Entities/ODProjector.m
@@ -47,8 +47,8 @@
     //aspectRatio = horizontalFOV / fov;
 
     fov = [ camera fov ];
-    //aspectRatio = [ camera aspectRatio ];
-    aspectRatio = horizontalFOV / fov;
+    aspectRatio = [ camera aspectRatio ];
+    //aspectRatio = horizontalFOV / fov;
 
     nearPlane = [ camera nearPlane ];
     farPlane  = [ camera farPlane  ];
@@ -56,9 +56,19 @@
     m4_mssss_projection_matrix(&projection, aspectRatio, fov, nearPlane, farPlane);
 }
 
+static const Vector3 frustumPPPositions[8] =
+{
+    {-1.0, -1.0, -1.0}, {1.0, -1.0, -1.0}, {1.0, 1.0, -1.0}, {-1.0, 1.0, -1.0},
+    {-1.0, -1.0,  1.0}, {1.0, -1.0,  1.0}, {1.0, 1.0,  1.0}, {-1.0, 1.0,  1.0}
+};
+
+static const uint32_t frustumPPEdges[24] =
+{
+    0, 1, 1, 2, 2, 3, 3, 0, 4, 5, 5, 6, 6, 7, 7, 4, 0, 4, 1, 5, 2, 6, 3, 7
+};
+
 - (void) updateView
 {
-    /*
     const Vector3 planeNormal = *NP_WORLD_Y_AXIS;
 
     Plane lowerPlane, basePlane, upperPlane;
@@ -75,13 +85,15 @@
     const double distanceToBasePlane  = plane_pv_signed_distance_from_plane(&basePlane,  &cameraPosition);
     const double distanceToUpperPlane = plane_pv_signed_distance_from_plane(&upperPlane, &cameraPosition);
 
-    NSLog(@"%lf %lf %lf", distanceToLowerPlane, distanceToBasePlane, distanceToUpperPlane);
+    //NSLog(@"%lf %lf %lf", distanceToLowerPlane, distanceToBasePlane, distanceToUpperPlane);
 
     const bool lookingDown = (dotNF < 0.0);
     const bool underWater  = (distanceToLowerPlane <  0.0 );
     const bool lowerSector = (distanceToLowerPlane >= 0.0 && distanceToBasePlane  < 0.0);
     const bool upperSector = (distanceToBasePlane  >= 0.0 && distanceToUpperPlane < 0.0);
     const bool outside     = (distanceToUpperPlane >= 0.0);
+
+    //NSLog(@"%d %d %d %d %d", (int)lookingDown, (int)underWater, (int)lowerSector, (int)upperSector, (int)outside);
 
     double move = 1.0;
 
@@ -93,7 +105,7 @@
 
     Vector3 moveUp = v3_sv_scaled(move, &planeNormal);
 
-    NSLog(@"%lf", move);
+    //NSLog(@"%lf", move);
 
     Vector3 direction;
 
@@ -122,7 +134,7 @@
     const Vector3 normalScaled = v3_sv_scaled(dotNForwardDirection, &planeNormal);
     const Vector3 aimTwo = v3_vv_sub(&forwardDirection, &normalScaled);
 
-    NSLog(@"%d\n%s\n%s", result, v3_v_to_string(&aimOne), v3_v_to_string(&aimTwo));
+    //NSLog(@"%d\n%s\n%s", result, v3_v_to_string(&aimOne), v3_v_to_string(&aimTwo));
 
     const double dotNFA = fabs(dotNF);
     const Vector3 ao = v3_sv_scaled(dotNFA, &aimOne);
@@ -130,7 +142,7 @@
 
     const Vector3 aim = v3_vv_add(&ao, &at);
 
-    NSLog(@"%s", v3_v_to_string(&aim));
+    //NSLog(@"%s", v3_v_to_string(&aim));
 
     position = v3_vv_add(&cameraPosition, &moveUp);
 
@@ -138,8 +150,121 @@
 
     Quaternion q = quat_m4_to_quaternion(&view);
     orientation = quat_q_conjugated(&q);
-    */
 
+    m4_mm_multiply_m(&projection, &view, &viewProjection);
+
+    const Matrix4 * const cameraInvVP = [ camera inverseViewProjection ];
+
+    Vector3 frustumWSPositions[8];
+    Vector3 intersectionPoints[24];
+    Vector3 projectedPoints[24];
+    Vector3 projectedPointsPP[24];
+    uint32_t n = 0;
+
+    for ( uint32_t i = 0; i < 8; i++ )
+    {
+        frustumWSPositions[i] = m4_mv3_multiply(cameraInvVP, &frustumPPPositions[i]);
+        //NSLog(@"%s", v3_v_to_string(&frustumWSPositions[i]));
+    }
+
+    for ( uint32_t i = 0; i < 12; i++ )
+    {
+        const uint32_t edgeBeginIndex = frustumPPEdges[i*2];
+        const uint32_t edgeEndIndex   = frustumPPEdges[i*2+1];
+
+        const Vector3 edgeBegin = frustumWSPositions[edgeBeginIndex];
+        const Vector3 edgeEnd   = frustumWSPositions[edgeEndIndex];
+
+        const double lowerBegin = plane_pv_signed_distance_from_plane(&lowerPlane, &edgeBegin);
+        const double lowerEnd   = plane_pv_signed_distance_from_plane(&lowerPlane, &edgeEnd);
+        const double upperBegin = plane_pv_signed_distance_from_plane(&upperPlane, &edgeBegin);
+        const double upperEnd   = plane_pv_signed_distance_from_plane(&upperPlane, &edgeEnd);
+
+        if (( lowerBegin / lowerEnd ) < 0.0 )
+        {
+            const int32_t result = plane_pvv_intersect_with_line_v(&lowerPlane, &edgeBegin, &edgeEnd, &intersectionPoints[n++]);
+            //NSLog(@"Low %u %u %d", edgeBeginIndex, edgeEndIndex, result);
+        }
+
+        if (( upperBegin / upperEnd ) < 0.0 )
+        {
+            const int32_t result = plane_pvv_intersect_with_line_v(&upperPlane, &edgeBegin, &edgeEnd, &intersectionPoints[n++]);
+            //NSLog(@"Up %u %u %d", edgeBeginIndex, edgeEndIndex, result);
+        }
+    }
+
+    for ( uint32_t i = 0; i < 8; i++ )
+    {
+        const double lower = plane_pv_signed_distance_from_plane(&lowerPlane, &frustumWSPositions[i]);
+        const double upper = plane_pv_signed_distance_from_plane(&upperPlane, &frustumWSPositions[i]);
+
+        if (( upper / lower ) < 0.0 )
+        {
+            intersectionPoints[n++] = frustumWSPositions[i];
+            //NSLog(@"F %u", i);
+        }
+    }
+
+    //NSLog(@"P %u", n);
+
+    if ( !n )
+    {
+        #warning do not render surface
+    }
+
+    for ( uint32_t i = 0; i < n; i++ )
+    {
+        const double dotPN = v3_vv_dot_product(&intersectionPoints[i], &planeNormal);
+        const Vector3 tmp = v3_sv_scaled(dotPN, &planeNormal);
+        projectedPoints[i] = v3_vv_sub(&intersectionPoints[i], &tmp);
+
+        //NSLog(@"%u %s", i, v3_v_to_string(&projectedPoints[i]));
+    }
+
+    for ( uint32_t i = 0; i < n; i++ )
+    {
+        projectedPointsPP[i] = m4_mv3_multiply(&viewProjection, &projectedPoints[i]);
+
+        //NSLog(@"PP %u %s", i, v3_v_to_string(&projectedPointsPP[i]));
+    }
+
+    double xMin = -1.0;
+    double xMax =  1.0;
+    double yMin = -1.0;
+    double yMax =  1.0;
+
+    if ( n!= 0)
+    {
+        xMin = projectedPointsPP[0].x;
+        xMax = projectedPointsPP[0].x;
+        yMin = projectedPointsPP[0].y;
+        yMax = projectedPointsPP[0].y;
+
+        for ( uint32_t i = 1; i < n; i++ )
+        {
+            xMin = MIN(projectedPointsPP[i].x, xMin);
+            xMax = MAX(projectedPointsPP[i].x, xMax);
+
+            yMin = MIN(projectedPointsPP[i].y, yMin);
+            yMax = MAX(projectedPointsPP[i].y, yMax);
+        }
+    }
+
+    //NSLog(@"%u %lf %lf %lf %lf", n, xMin, xMax, yMin, yMax);
+
+    Matrix4 invVP;
+    m4_m_inverse_m(&viewProjection, &invVP);
+
+    m4_m_set_identity(&range);
+
+    M_EL(range, 0, 0) = 0.5 * (xMax - xMin);
+    M_EL(range, 1, 1) = 0.5 * (yMax - yMin);
+    M_EL(range, 3, 0) = 0.5 * (xMax - xMin) + xMin;
+    M_EL(range, 3, 1) = 0.5 * (yMax - yMin) + yMin;
+
+    m4_mm_multiply_m(&invVP, &range, &inverseViewProjection);
+
+    /*
     yaw   = fmod(yaw,   360.0);
     pitch = fmod(pitch, 360.0);
 
@@ -209,6 +334,7 @@
     Matrix4 rotate = quat_q_to_matrix4(&q);
     Matrix4 translate = m4_v_translation_matrix(&invpos);
     m4_mm_multiply_m(&rotate, &translate, &view);
+    */
 }
 
 @end
@@ -436,8 +562,8 @@ static NPInputAction * create_input_action(NSString * projectorName, NSString * 
     [ self updateProjection ];
     [ self updateView ];
 
-    m4_mm_multiply_m(&projection, &view, &viewProjection);
-    m4_m_inverse_m(&viewProjection, &inverseViewProjection);
+//    m4_mm_multiply_m(&projection, &view, &viewProjection);
+//    m4_m_inverse_m(&viewProjection, &inverseViewProjection);
 
     connectedToCameraLastFrame = connectedToCamera;
 }
