3492d3d977ea79ec783808a867450501f2c2e623
Thomas Gamper <1222479+eiszapfen2000@users.noreply.github.com>
2014-02-28 17:10:54 +0000

    TG: - PiersonMoskovitzSpectrum.m do proper integration domain conversion - h0.m hack to use pierson moskovitz - h0_x.m more hacks to transform pierson moskovitz and visualize

--- Trunk/Matlab/h0.m
+++ Trunk/Matlab/h0.m
@@ -1,11 +1,18 @@
 function y = h0(k, knorm, knormalised, randomnumbers, wind, A, l)
 
-phillipsspectrum = PhillipsSpectrum(k, knorm, knormalised, wind, A, l);
+%phillipsspectrum = PhillipsSpectrum(k, knorm, knormalised, wind, A, l);
 %brakdibrak = UnifiedSpectrum(k, knorm, knormalised, wind, A, l);
 %brakdibrak(isnan(brakdibrak)) = 0.0;
 
-tmp = complex((1/sqrt(2)));
-tmp = tmp.*randomnumbers;
-tmp = tmp.*complex(sqrt(phillipsspectrum));
+pmspectrum = PiersonMoskovitzSpectrum(k, knorm, knormalised, wind, A, l);
+
+%tmp = complex((1/sqrt(2)));
+%tmp = tmp.*randomnumbers;
+%tmp = tmp.*complex(sqrt(phillipsspectrum));
 %tmp = tmp.*complex(sqrt(brakdibrak));
-y = tmp;
+%y = tmp;
+
+y = randomnumbers .* complex(sqrt(pmspectrum));
+
+end
+
