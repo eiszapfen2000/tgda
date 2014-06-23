cf050c0cfecec849d13599fa1cba99ce361f3a75
icicle <icicle@f832cba2-7736-0410-a208-86f5be76f6c5>
2014-06-23 16:23:21 +0000

    TG: - Donelan19851D.m add

---
+++ Trunk/Matlab/Donelan19851D.m
@@ -0,0 +1,64 @@
+function y = Donelan19851D(omega, wind, fetch, scale)
+
+g = 9.81;
+U10 = norm(wind);
+
+% non dimensional fetch X = g * fetch / (U10*U10)
+% non dimensional peak frequency v = f_p * U10 / g
+% f_p = v * g / U10
+% w_p = 2 * pi * f_p
+
+% Omega_c = U10 / C_p = 11.6 * X^-0.23
+% v = 1.85 * X^-0.23
+
+X = g * fetch / (U10^2);
+v = 1.85 * (X^(-0.23));
+Omega_c = 11.6 * (X^(-0.23));
+f_p = v * g / U10;
+
+if Omega_c >= 5 || Omega_c < 0.82
+    error('U10/C_p out of range (0.82, 5)');
+end
+
+alpha = 0.006 * (Omega_c^(0.55));
+omega_p = 2 * pi * f_p;
+
+% alpha = 0.076 * power((U^2) / (fetch*g), 0.22);
+% omega_p = 22 * power((g*g) / (U*fetch), 1/3);
+
+alpha = alpha .* scale.alphaScale;
+omega_p = omega_p .* scale.wpScale;
+
+% sigma = zeros(size(omega));
+% sigma(omega <= omega_p) = 0.07;
+% sigma(omega > omega_p) = 0.09;
+
+sigma = 0.08 * (1 + (4/(Omega_c^3)));
+
+if scale.sigma ~= 0
+    sigma = scale.sigma;
+end
+
+
+gamma_base = 0.0;
+if Omega_c < 1 
+    gamma_base = 1.7;
+else
+    gamma_base = 1.7 + 6 * log(Omega_c);
+end
+
+r_exponent = -((omega-omega_p).^2)./(2.*(sigma.^2).*(omega_p^2));
+r = exp(r_exponent);
+gamma_r = gamma_base .^ r;
+
+if scale.gamma ~= 0
+    gamma_r = scale.gamma;
+end
+
+omega(omega==0) = Inf;
+exponent = -power(omega_p ./ omega, 4.0);
+Theta = ((alpha*g*g) ./ (power(omega, 4.0) .* omega_p)) .* exp(exponent) .* gamma_r;
+
+y = Theta;
+
+end
