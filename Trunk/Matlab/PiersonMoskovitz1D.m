76bdd2f1ce724875eba67a55a2467efe7a668e0e
Thomas Gamper <1222479+eiszapfen2000@users.noreply.github.com>
2014-06-25 15:47:33 +0000

    TG: - PiersonMoskovitz1D.m parameter handling - PiersonMoskovitz1Dk.m adapt to above change - pm_plots.m likewise

--- Trunk/Matlab/PiersonMoskovitz1D.m
+++ Trunk/Matlab/PiersonMoskovitz1D.m
@@ -1,9 +1,30 @@
-function y = PiersonMoskovitz1D(omega, wind, alphaScale, wpScale)
+function y = PiersonMoskovitz1D(omega, wind, parameters)
 
 g = 9.81;
+
+alphaScale = 1;
+wpScale = 1;
+wp = 0;
+
+if isfield(parameters, 'alphaScale') && isnumeric(parameters.alphaScale)
+alphaScale = parameters.alphaScale;
+end
+
+if isfield(parameters, 'wpScale') && isnumeric(parameters.wpScale)
+wpScale = parameters.wpScale;
+end
+
+if isfield(parameters, 'wp') && isnumeric(parameters.wp)
+wp = parameters.wp;
+end
+
 alpha = 0.0081 * alphaScale;
 U = norm(wind);
 omega_p = (0.855 * g / U) * wpScale;
+
+if wp ~= 0
+    omega_p = wp;
+end
 
 omega(omega==0) = Inf;
 
