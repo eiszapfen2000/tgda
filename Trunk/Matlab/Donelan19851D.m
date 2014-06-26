1dbb7325d5e26ddbfc9ca42042a433e32a2b89d2
Thomas Gamper <1222479+eiszapfen2000@users.noreply.github.com>
2014-06-26 14:36:10 +0000

    TG:  - Donelan19851D.m parameter handling

--- Trunk/Matlab/Donelan19851D.m
+++ Trunk/Matlab/Donelan19851D.m
@@ -1,7 +1,35 @@
-function y = Donelan19851D(omega, wind, fetch, scale)
+function y = Donelan19851D(omega, wind, fetch, parameters)
 
 g = 9.81;
-U10 = norm(wind);
+
+alphaScale = 1;
+wpScale = 1;
+fixed_gamma_r = 0;
+fixed_sigma = 0;
+fixed_omega_p = 0;
+fixed_omega_c = 0;
+
+if isfield(parameters, 'alphaScale') && isnumeric(parameters.alphaScale)
+    alphaScale = parameters.alphaScale;
+end
+
+if isfield(parameters, 'wpScale') && isnumeric(parameters.wpScale)
+    wpScale = parameters.wpScale;
+end
+
+if isfield(parameters, 'gamma_r') && isnumeric(parameters.gamma_r)
+    fixed_gamma_r = parameters.gamma_r;
+end
+
+if isfield(parameters, 'sigma') && isnumeric(parameters.sigma)
+    fixed_sigma = parameters.sigma;
+end
+
+if isfield(parameters, 'wp') && isnumeric(parameters.wp)...
+   && isfield(parameters, 'wc') && isnumeric(parameters.wc)
+    fixed_omega_p = parameters.wp;
+    fixed_omega_c = parameters.wc;
+end
 
 % non dimensional fetch X = g * fetch / (U10*U10)
 % non dimensional peak frequency v = f_p * U10 / g
@@ -12,20 +40,31 @@ U10 = norm(wind);
 % v = 1.85 * X^-0.23
 % U10 / C_p = 2 *pi * v
 
-X = g * fetch / (U10^2);
-Omega_c = 11.6 * (X^(-0.23));
-Omega_c = max(min(Omega_c, 5), 0.83);
+alpha = 0;
+Omega_c = 0;
+omega_p = 0;
 
-alpha = 0.006 * (Omega_c^(0.55));
-omega_p = Omega_c * g / U10;
-
-alpha = alpha .* scale.alphaScale;
-omega_p = omega_p .* scale.wpScale;
+if fixed_omega_p == 0 && fixed_omega_c == 0
+    U10 = norm(wind);
+    X = g * fetch / (U10^2);
+    Omega_c = 11.6 * (X^(-0.23));
+    % Omega_c = max(min(Omega_c, 5), 0.83);
+    alpha = 0.006 * (Omega_c^(0.55));
+    omega_p = Omega_c * g / U10;
+    alpha = alpha .* alphaScale;
+    omega_p = omega_p .* wpScale;
+else
+    U10 = fixed_omega_c * g / fixed_omega_p;
+    X = nthroot(fixed_omega_c / 11.6, -0.23);
+    alpha = 0.006 * (fixed_omega_c^(0.55));
+    Omega_c = fixed_omega_c;
+    omega_p = fixed_omega_p;
+end
 
 sigma = 0.08 * (1 + (4/(Omega_c^3)));
 
-if scale.sigma ~= 0
-    sigma = scale.sigma;
+if fixed_sigma ~= 0
+    sigma = fixed_sigma;
 end
 
 gamma_base = 0.0;
@@ -39,8 +78,8 @@ r_exponent = -((omega-omega_p).^2)./(2.*(sigma.^2).*(omega_p^2));
 r = exp(r_exponent);
 gamma_r = gamma_base .^ r;
 
-if scale.gamma ~= 0
-    gamma_r = scale.gamma;
+if fixed_gamma_r ~= 0
+    gamma_r = fixed_gamma_r;
 end
 
 omega(omega==0) = Inf;
