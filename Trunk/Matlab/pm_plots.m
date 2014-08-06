9207de3583c4e6d44d739c8d4a10215b2229b023
icicle <icicle@f832cba2-7736-0410-a208-86f5be76f6c5>
2014-08-06 10:19:20 +0000

    TG: - PiersonMoskovitz1Dk.m fix potential NaN - pm_plots.m rewrite with large range for omega, we will clamp the range displayed in TeX

--- Trunk/Matlab/pm_plots.m
+++ Trunk/Matlab/pm_plots.m
@@ -3,12 +3,8 @@ close all
 g = 9.81;
 
 %1d spectra
-omega = [0:0.005:pi/2];
+omega = 0:0.005:2*pi;
 k = (omega .* omega) ./ g;
-
-pm_parameters = [];
-pm_parameters.alphaScale = 1;
-pm_parameters.wpScale = 1;
 
 pm_10 = PiersonMoskovitz1D(omega, 10.0, []);
 pm_12 = PiersonMoskovitz1D(omega, 12.5, []);
@@ -34,6 +30,31 @@ pm_15_k = PiersonMoskovitz1Dk(k, 15.0);
 pm_17_k = PiersonMoskovitz1Dk(k, 17.5);
 pm_20_k = PiersonMoskovitz1Dk(k, 20.0);
 
+write2dcsv(omega, pm_10);
+write2dcsv(omega, pm_12);
+write2dcsv(omega, pm_15);
+write2dcsv(omega, pm_17);
+write2dcsv(omega, pm_20);
+
+write2dcsv(omega, pm_15_alpha_07);
+write2dcsv(omega, pm_15_alpha_08);
+write2dcsv(omega, pm_15_alpha_09);
+write2dcsv(omega, pm_15_alpha_11);
+write2dcsv(omega, pm_15_alpha_12);
+write2dcsv(omega, pm_15_alpha_13);
+
+write2dcsv(omega, pm_15_omegap_08);
+write2dcsv(omega, pm_15_omegap_09);
+write2dcsv(omega, pm_15_omegap_11);
+write2dcsv(omega, pm_15_omegap_12);
+write2dcsv(omega, pm_15_omegap_13);
+
+write2dcsv(omega, pm_10_k);
+write2dcsv(omega, pm_12_k);
+write2dcsv(omega, pm_15_k);
+write2dcsv(omega, pm_17_k);
+write2dcsv(omega, pm_20_k);
+
 % figure;
 % hold on;
 % plot(omega, pm_15);
@@ -46,53 +67,3 @@ pm_20_k = PiersonMoskovitz1Dk(k, 20.0);
 % % plot(omega, pm_17);
 % % plot(omega, pm_20);
 % hold off;
-
-o_pm_10 = [omega', pm_10'];
-o_pm_12 = [omega', pm_12'];
-o_pm_15 = [omega', pm_15'];
-o_pm_17 = [omega', pm_17'];
-o_pm_20 = [omega', pm_20'];
-
-o_pm_15_alpha_07 = [omega', pm_15_alpha_07'];
-o_pm_15_alpha_08 = [omega', pm_15_alpha_08'];
-o_pm_15_alpha_09 = [omega', pm_15_alpha_09'];
-o_pm_15_alpha_11 = [omega', pm_15_alpha_11'];
-o_pm_15_alpha_12 = [omega', pm_15_alpha_12'];
-o_pm_15_alpha_13 = [omega', pm_15_alpha_13'];
-
-o_pm_15_omegap_08 = [omega', pm_15_omegap_08'];
-o_pm_15_omegap_09 = [omega', pm_15_omegap_09'];
-o_pm_15_omegap_11 = [omega', pm_15_omegap_11'];
-o_pm_15_omegap_12 = [omega', pm_15_omegap_12'];
-o_pm_15_omegap_13 = [omega', pm_15_omegap_13'];
-
-k_pm_10 = [k', pm_10_k'];
-k_pm_12 = [k', pm_12_k'];
-k_pm_15 = [k', pm_15_k'];
-k_pm_17 = [k', pm_17_k'];
-k_pm_20 = [k', pm_20_k'];
-
-% csvwrite('pm_10.dat', o_pm_10);
-% csvwrite('pm_12.dat', o_pm_12);
-% csvwrite('pm_15.dat', o_pm_15);
-% csvwrite('pm_17.dat', o_pm_17);
-% csvwrite('pm_20.dat', o_pm_20);
-
-% csvwrite('pm_15_alpha_07.dat', o_pm_15_alpha_07);
-% csvwrite('pm_15_alpha_08.dat', o_pm_15_alpha_08);
-% csvwrite('pm_15_alpha_09.dat', o_pm_15_alpha_09);
-% csvwrite('pm_15_alpha_11.dat', o_pm_15_alpha_11);
-% csvwrite('pm_15_alpha_12.dat', o_pm_15_alpha_12);
-% csvwrite('pm_15_alpha_13.dat', o_pm_15_alpha_13);
-
-% csvwrite('pm_15_omegap_08.dat', o_pm_15_omegap_08);
-% csvwrite('pm_15_omegap_09.dat', o_pm_15_omegap_09);
-% csvwrite('pm_15_omegap_11.dat', o_pm_15_omegap_11);
-% csvwrite('pm_15_omegap_12.dat', o_pm_15_omegap_12);
-% csvwrite('pm_15_omegap_13.dat', o_pm_15_omegap_13);
-
-% csvwrite('pm_10_k.dat', k_pm_10);
-% csvwrite('pm_12_k.dat', k_pm_12);
-% csvwrite('pm_15_k.dat', k_pm_15);
-% csvwrite('pm_17_k.dat', k_pm_17);
-% csvwrite('pm_20_k.dat', k_pm_20);
