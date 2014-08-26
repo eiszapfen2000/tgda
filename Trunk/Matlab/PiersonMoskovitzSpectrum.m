16be4e7187ef4515fe42251c05195817f43133be
icicle <icicle@f832cba2-7736-0410-a208-86f5be76f6c5>
2014-08-26 13:27:57 +0000

    TG: - PiersonMoskovitzSpectrum.m some hackery to generate two directional spreads with the same parameters - directional_plots.m use above hackery, save directional spreads as PNG with 96PPI

--- Trunk/Matlab/PiersonMoskovitzSpectrum.m
+++ Trunk/Matlab/PiersonMoskovitzSpectrum.m
@@ -1,4 +1,4 @@
-function [s d] = PiersonMoskovitzSpectrum(k, knorm, wind)
+function [s dm dd] = PiersonMoskovitzSpectrum(k, knorm, wind)
 
 g = 9.81;
 
@@ -7,7 +7,8 @@ theta = atan2(k(:,:,2), k(:,:,1));
 theta_w = atan2(wind(2), wind(1));
 
 [Theta omega_p] = PiersonMoskovitz1D(omega, wind, []);
-df = DirectionalFilter(omega, omega_p, theta, theta_w);
+df_m = DirectionalFilter(omega, omega_p, theta, theta_w);
+df_d = DonelanDirectionalFilter(omega, omega_p, theta, theta_w);
 
 kn = knorm;
 kn(kn==0) = Inf;
@@ -15,7 +16,9 @@ omega(omega==0) = Inf;
 
 Theta_k = Theta .* 0.5 .* (g ./ omega) ./ kn;
 
-s = Theta_k .* df;
-d = df;
+s = Theta_k .* df_m;
+
+dm = df_m;
+dd = df_d;
 
 end
