7f97c90e788b786fda63ad23f31ddf42f2eddb3f
Thomas Gamper <1222479+eiszapfen2000@users.noreply.github.com>
2014-09-22 16:19:49 +0000

    TG: - build_wave_vectors.m add

---
+++ Trunk/Matlab/build_wave_vectors.m
@@ -0,0 +1,20 @@
+function [k knorm x] = build_wave_vectors(resolution, area)
+
+x = zeros(resolution(1), resolution(2), 2);
+k = zeros(resolution(1), resolution(2), 2);
+alphas = -resolution(1)/2:1:resolution(1)/2-1;
+betas  =  resolution(2)/2:-1:-(resolution(2)/2)+1;
+
+deltax = area(1) / resolution(1);
+deltay = area(2) / resolution(2);
+deltakx = 2*pi / area(1);
+deltaky = 2*pi / area(2);
+
+% spatial domain
+[ x(:,:,1) x(:,:,2) ] = meshgrid(alphas.*deltax, betas.*deltay);
+% wave vector domain
+[ k(:,:,1) k(:,:,2) ] = meshgrid(alphas.*deltakx, betas.*deltaky);
+
+knorm = sqrt(sum(abs(k).^2, 3));
+
+end
