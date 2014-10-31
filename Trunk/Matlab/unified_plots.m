46603749fee24265918486d2f832578670cbe6cc
Thomas Gamper <1222479+eiszapfen2000@users.noreply.github.com>
2014-10-31 13:35:30 +0000

    TG: - unified_plots.m more plot exports - background.tex add plot - u_*_500km_k.dat add plot data

--- Trunk/Matlab/unified_plots.m
+++ Trunk/Matlab/unified_plots.m
@@ -7,17 +7,6 @@ k = (omega .* omega) ./ g;
 
 pm_10_k = PiersonMoskovitz1Dk(k, 10.0, []);
 
-u_3_500km_k = UnifiedSpectrum1Dk(k, 3.0, 500000, []);
-u_5_500km_k= UnifiedSpectrum1Dk(k, 5.0, 500000, []);
-u_7_500km_k = UnifiedSpectrum1Dk(k, 7.0, 500000, []);
-u_9_500km_k = UnifiedSpectrum1Dk(k, 9.0, 500000, []);
-u_11_500km_k = UnifiedSpectrum1Dk(k, 11.0, 500000, []);
-u_13_500km_k = UnifiedSpectrum1Dk(k, 13.0, 500000, []);
-u_15_500km_k = UnifiedSpectrum1Dk(k, 15.0, 500000, []);
-u_17_500km_k = UnifiedSpectrum1Dk(k, 17.0, 500000, []);
-u_19_500km_k = UnifiedSpectrum1Dk(k, 19.0, 500000, []);
-u_21_500km_k = UnifiedSpectrum1Dk(k, 21.0, 500000, []);
-
 u_10_5km_k = UnifiedSpectrum1Dk(k, 10.0, 5000, []);
 u_10_10km_k = UnifiedSpectrum1Dk(k, 10.0, 10000, []);
 u_10_25km_k = UnifiedSpectrum1Dk(k, 10.0, 25000, []);
@@ -78,20 +67,111 @@ d_10_400km_k = Donelan19851Dk(k, 10.0, 400000, []);
 d_10_500km_k = Donelan19851Dk(k, 10.0, 500000, []);
 d_10_1000km_k = Donelan19851Dk(k, 10.0, 1000000, []);
 
-% figure
-% hold on
-% plot(log10(k), log10(u_3_500km_k), 'r');
-% plot(log10(k), log10(u_5_500km_k), 'r');
-% plot(log10(k), log10(u_7_500km_k), 'r');
-% plot(log10(k), log10(u_9_500km_k), 'r');
-% plot(log10(k), log10(u_11_500km_k), 'r');
-% plot(log10(k), log10(u_13_500km_k), 'r');
-% plot(log10(k), log10(u_15_500km_k), 'r');
-% plot(log10(k), log10(u_17_500km_k), 'r');
-% plot(log10(k), log10(u_19_500km_k), 'r');
-% plot(log10(k), log10(u_21_500km_k), 'r');
-% axis([-3, 4, -16, 5]);
-% hold off
+% k = 0:0.5:10^3;
+%k = 0:0.001:1;
+%k = [k, 1.5:0.5:10^3];
+k = logspace(-3, 4, 1000);
+b = k .^ 3;
+
+u_3_500km_k = UnifiedSpectrum1Dk(k, 3.0, 500000, []);
+u_5_500km_k= UnifiedSpectrum1Dk(k, 5.0, 500000, []);
+u_7_500km_k = UnifiedSpectrum1Dk(k, 7.0, 500000, []);
+u_9_500km_k = UnifiedSpectrum1Dk(k, 9.0, 500000, []);
+u_11_500km_k = UnifiedSpectrum1Dk(k, 11.0, 500000, []);
+u_13_500km_k = UnifiedSpectrum1Dk(k, 13.0, 500000, []);
+u_15_500km_k = UnifiedSpectrum1Dk(k, 15.0, 500000, []);
+u_17_500km_k = UnifiedSpectrum1Dk(k, 17.0, 500000, []);
+u_19_500km_k = UnifiedSpectrum1Dk(k, 19.0, 500000, []);
+u_21_500km_k = UnifiedSpectrum1Dk(k, 21.0, 500000, []);
+
+write2dcsv(k, u_3_500km_k);
+write2dcsv(k, u_5_500km_k);
+write2dcsv(k, u_7_500km_k);
+write2dcsv(k, u_9_500km_k);
+write2dcsv(k, u_11_500km_k);
+write2dcsv(k, u_13_500km_k);
+write2dcsv(k, u_15_500km_k);
+write2dcsv(k, u_17_500km_k);
+write2dcsv(k, u_19_500km_k);
+write2dcsv(k, u_21_500km_k);
+
+u_d_3_500km_k = Donelan19851Dk(k, 3.0, 500000, []);
+u_d_5_500km_k= Donelan19851Dk(k, 5.0, 500000, []);
+u_d_7_500km_k = Donelan19851Dk(k, 7.0, 500000, []);
+u_d_9_500km_k = Donelan19851Dk(k, 9.0, 500000, []);
+u_d_11_500km_k = Donelan19851Dk(k, 11.0, 500000, []);
+u_d_13_500km_k = Donelan19851Dk(k, 13.0, 500000, []);
+u_d_15_500km_k = Donelan19851Dk(k, 15.0, 500000, []);
+u_d_17_500km_k = Donelan19851Dk(k, 17.0, 500000, []);
+u_d_19_500km_k = Donelan19851Dk(k, 19.0, 500000, []);
+u_d_21_500km_k = Donelan19851Dk(k, 21.0, 500000, []);
+
+write2dcsv(k, u_d_3_500km_k);
+write2dcsv(k, u_d_5_500km_k);
+write2dcsv(k, u_d_7_500km_k);
+write2dcsv(k, u_d_9_500km_k);
+write2dcsv(k, u_d_11_500km_k);
+write2dcsv(k, u_d_13_500km_k);
+write2dcsv(k, u_d_15_500km_k);
+write2dcsv(k, u_d_17_500km_k);
+write2dcsv(k, u_d_19_500km_k);
+write2dcsv(k, u_d_21_500km_k);
+
+figure
+hold on
+plot(log10(k), log10(u_3_500km_k), 'r');
+plot(log10(k), log10(u_5_500km_k), 'r');
+plot(log10(k), log10(u_7_500km_k), 'r');
+plot(log10(k), log10(u_9_500km_k), 'r');
+plot(log10(k), log10(u_11_500km_k), 'r');
+plot(log10(k), log10(u_13_500km_k), 'r');
+plot(log10(k), log10(u_15_500km_k), 'r');
+plot(log10(k), log10(u_17_500km_k), 'r');
+plot(log10(k), log10(u_19_500km_k), 'r');
+plot(log10(k), log10(u_21_500km_k), 'r');
+
+plot(log10(k), log10(u_d_3_500km_k), 'b');
+plot(log10(k), log10(u_d_5_500km_k), 'b');
+plot(log10(k), log10(u_d_7_500km_k), 'b');
+plot(log10(k), log10(u_d_9_500km_k), 'b');
+plot(log10(k), log10(u_d_11_500km_k), 'b');
+plot(log10(k), log10(u_d_13_500km_k), 'b');
+plot(log10(k), log10(u_d_15_500km_k), 'b');
+plot(log10(k), log10(u_d_17_500km_k), 'b');
+plot(log10(k), log10(u_d_19_500km_k), 'b');
+plot(log10(k), log10(u_d_21_500km_k), 'b');
+
+axis([-3, 5, -16, 5]);
+hold off
+
+figure
+hold on
+plot(log10(k), log10(u_3_500km_k .* b), 'r');
+plot(log10(k), log10(u_5_500km_k .* b), 'r');
+plot(log10(k), log10(u_7_500km_k .* b), 'r');
+plot(log10(k), log10(u_9_500km_k .* b), 'r');
+plot(log10(k), log10(u_11_500km_k .* b), 'r');
+plot(log10(k), log10(u_13_500km_k .* b), 'r');
+plot(log10(k), log10(u_15_500km_k .* b), 'r');
+plot(log10(k), log10(u_17_500km_k .* b), 'r');
+plot(log10(k), log10(u_19_500km_k .* b), 'r');
+plot(log10(k), log10(u_21_500km_k .* b), 'r');
+
+plot(log10(k), log10(u_d_3_500km_k .* b), 'b');
+plot(log10(k), log10(u_d_5_500km_k .* b), 'b');
+plot(log10(k), log10(u_d_7_500km_k .* b), 'b');
+plot(log10(k), log10(u_d_9_500km_k .* b), 'b');
+plot(log10(k), log10(u_d_11_500km_k .* b), 'b');
+plot(log10(k), log10(u_d_13_500km_k .* b), 'b');
+plot(log10(k), log10(u_d_15_500km_k .* b), 'b');
+plot(log10(k), log10(u_d_17_500km_k .* b), 'b');
+plot(log10(k), log10(u_d_19_500km_k .* b), 'b');
+plot(log10(k), log10(u_d_21_500km_k .* b), 'b');
+
+axis([-3, 4, -4, 0]);
+hold off
+
+
 
 % figure
 % hold on
