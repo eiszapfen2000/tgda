9c44cb20a5bd3bbb2bcde97aa2643e73b5de754f
icicle <icicle@f832cba2-7736-0410-a208-86f5be76f6c5>
2015-01-23 10:56:16 +0000

    TG: - ocean_init.m add

---
+++ Trunk/Matlab/ocean_init.m
@@ -0,0 +1,66 @@
+function result = ocean_init(geometry, settings)
+
+if ~isscalar(geometry.geometryRes) || ~isscalar(geometry.gradientRes)
+    error('BRAK');
+end
+
+geometryResLog2 = log2(geometry.geometryRes);
+gradientResLog2 = log2(geometry.gradientRes);
+
+[ ige fge ] = modf(geometryResLog2);
+[ igr fgr ] = modf(gradientResLog2);
+
+if ~isequal(fge, 0) || ~isequal(fgr, 0)
+    error('Resolution not power of 2');
+end
+
+if isempty(geometry.lodAreas)
+    error('LOLZ');
+end
+
+result.lods = [];
+necessaryRes = max(geometry.geometryRes, geometry.gradientRes);
+numberOfLods = size(geometry.lodAreas, 1);
+
+for l=1:numberOfLods
+    [k kn deltakx deltaky] = generate_k(necessaryRes, geometry.lodAreas(l));
+    [x z deltax deltay]  = generate_xz(necessaryRes, geometry.lodAreas(l));
+    
+    Theta = generateSpectrum(k, kn, settings);
+    amplitudes = sqrt(2.*Theta.*deltakx.*deltaky);
+    
+    gaussrandr = normrnd(0, 1, necessaryRes, necessaryRes);
+    gaussrandi = normrnd(0, 1, necessaryRes, necessaryRes);
+    
+    result.lods(l).resolution = necessaryRes;
+    result.lods(1).area = geometry.lodAreas(l);
+    result.lods(l).k = k;
+    result.lods(l).kn = kn;
+    result.lods(l).deltakx = deltakx;
+    result.lods(l).deltaky = deltaky;
+    result.lods(l).x = x;
+    result.lods(l).z = z;
+    result.lods(l).deltax = deltax;
+    result.lods(l).deltay = deltay;
+    result.lods(l).Theta = Theta;
+    result.lods(l).amplitudes = amplitudes;
+    result.lods(l).randomNumbers = complex(gaussrandr, gaussrandi);
+end
+
+end
+
+function y = generateSpectrum(k, kn, settings)
+
+switch lower(settings.generatorName)
+    case 'pm'
+        y = PiersonMoskovitzSpectrum(k, kn, settings.wind);
+    case 'jonswap'
+        y = JONSWAPSpectrum(k, kn, settings.wind, settings.fetch);
+    case 'donelan'
+        y = DonelanSpectrum(k, kn, settings.wind, settings.fetch);
+    case 'unified'
+        y = UnifiedSpectrum(k, kn, settings.wind, settings.fetch);
+end
+
+end
+
