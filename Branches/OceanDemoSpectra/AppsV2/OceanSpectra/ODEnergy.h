cf15a61fc20f49c2cbbb2f9f051970c137f053fa
icicle <icicle@f832cba2-7736-0410-a208-86f5be76f6c5>
2015-02-12 13:20:27 +0000

    TG: ODPFrequencySpectrumGeneration - add JONSWAP related settings

--- Branches/OceanDemoSpectra/AppsV2/OceanSpectra/ODEnergy.h
+++ Branches/OceanDemoSpectra/AppsV2/OceanSpectra/ODEnergy.h
@@ -2,7 +2,7 @@
 #define ODENERGY_H_
 
 float peak_energy_wave_frequency_pm(float U10);
-float peak_energy_wave_frequency_jonswap(float U10);
+float peak_energy_wave_frequency_jonswap(float U10, float fetch);
 
 float energy_pm_wave_frequency(float omega, float U10);
 float energy_jonswap_wave_frequency(float omega, float U10, float fetch);
