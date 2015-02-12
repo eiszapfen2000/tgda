2e9cafc48a2df4f840a74b6a13aa713c0a253298
Thomas Gamper <1222479+eiszapfen2000@users.noreply.github.com>
2015-02-12 14:40:00 +0000

    TG: ODEnergy - start adding Donelan spectrum stuff

--- Branches/OceanDemoSpectra/AppsV2/OceanSpectra/ODEnergy.h
+++ Branches/OceanDemoSpectra/AppsV2/OceanSpectra/ODEnergy.h
@@ -3,11 +3,14 @@
 
 float peak_energy_wave_frequency_pm(float U10);
 float peak_energy_wave_frequency_jonswap(float U10, float fetch);
+float peak_energy_wave_frequency_donelan(float U10, float fetch);
 
 float energy_pm_wave_frequency(float omega, float U10);
 float energy_jonswap_wave_frequency(float omega, float U10, float fetch);
+float energy_donelan_wave_frequency(float omega, float U10, float fetch);
 
 float directional_spreading_mitsuyasu_hasselmann(float omega_p, float omega, float theta_p, float theta);
+float directional_spreading_donelan(float omega_p, float omega, float theta_p, float theta);
 
 #endif
 
