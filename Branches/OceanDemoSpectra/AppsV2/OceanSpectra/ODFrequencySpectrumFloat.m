eee22490ab24ed4842b5a172fe4a05338d9fffc5
icicle <icicle@f832cba2-7736-0410-a208-86f5be76f6c5>
2015-02-14 12:14:09 +0000

    TG: Work on Donelan mss computation, this is more tricky because of non-linear omega-k dependency

--- Branches/OceanDemoSpectra/AppsV2/OceanSpectra/ODFrequencySpectrumFloat.m
+++ Branches/OceanDemoSpectra/AppsV2/OceanSpectra/ODFrequencySpectrumFloat.m
@@ -48,6 +48,10 @@ ODQuadrants;
 
     const float omega_p = peak_energy_wave_frequency_pm(U10);
 
+    float mssX  = 0.0;
+    float mssY  = 0.0;
+    float mssXY = 0.0;
+
     for ( int32_t l = 0; l < numberOfLods; l++ )
     {
         const FVector2 lastSize
@@ -104,11 +108,15 @@ ODQuadrants;
                     Theta_complete = Theta_wavevector * directionalSpread;
                 }
 
+                mssX  += (kx * kx) * (dkx * dky) * Theta_complete;
+                mssY  += (ky * ky) * (dkx * dky) * Theta_complete;
+                mssXY += (kx * kx + ky * ky) * (dkx * dky) * Theta_complete;
+
                 const float amplitude = sqrtf(2.0f * Theta_complete * dkx * dky);
 
                 baseSpectrum[index] = Theta_complete;
-                H0[index][0] = MATH_1_DIV_SQRT_2f * /*xi_r **/ amplitude * 0.5f;
-                H0[index][1] = MATH_1_DIV_SQRT_2f * /*xi_i **/ amplitude * 0.5f;
+                H0[index][0] = MATH_1_DIV_SQRT_2f * xi_r * amplitude * 0.5f;
+                H0[index][1] = MATH_1_DIV_SQRT_2f * xi_i * amplitude * 0.5f;
 
                 printf("%+f %+fi ", H0[index][0], H0[index][1]);
             }
@@ -139,6 +147,10 @@ ODQuadrants;
     const float m =  (fresolution.y / 2.0f);
 
     const float omega_p = peak_energy_wave_frequency_jonswap(U10, fetch);
+
+    float mssX  = 0.0;
+    float mssY  = 0.0;
+    float mssXY = 0.0;
 
     for ( int32_t l = 0; l < numberOfLods; l++ )
     {
@@ -196,11 +208,15 @@ ODQuadrants;
                     Theta_complete = Theta_wavevector * directionalSpread;
                 }
 
+                mssX  += (kx * kx) * (dkx * dky) * Theta_complete;
+                mssY  += (ky * ky) * (dkx * dky) * Theta_complete;
+                mssXY += (kx * kx + ky * ky) * (dkx * dky) * Theta_complete;
+
                 const float amplitude = sqrtf(2.0f * Theta_complete * dkx * dky);
 
                 baseSpectrum[index] = Theta_complete;
-                H0[index][0] = MATH_1_DIV_SQRT_2f * /*xi_r **/ amplitude * 0.5f;
-                H0[index][1] = MATH_1_DIV_SQRT_2f * /*xi_i **/ amplitude * 0.5f;
+                H0[index][0] = MATH_1_DIV_SQRT_2f * xi_r * amplitude * 0.5f;
+                H0[index][1] = MATH_1_DIV_SQRT_2f * xi_i * amplitude * 0.5f;
 
                 printf("%+f %+fi ", H0[index][0], H0[index][1]);
             }
@@ -212,8 +228,8 @@ ODQuadrants;
 
 - (void) generateDonelanSpectrum
 {
-    const OdJONSWAPGeneratorSettings settings
-        = currentGeneratorSettings.jonswap;
+    const OdDonelanGeneratorSettings settings
+        = currentGeneratorSettings.donelan;
 
     const IVector2 resolution  = H0Resolution;
     const FVector2 fresolution = fv2_v_from_iv2(&resolution);
@@ -231,6 +247,10 @@ ODQuadrants;
     const float m =  (fresolution.y / 2.0f);
 
     const float omega_p = peak_energy_wave_frequency_donelan(U10, fetch);
+
+    float mssX  = 0.0;
+    float mssY  = 0.0;
+    float mssXY = 0.0;
 
     for ( int32_t l = 0; l < numberOfLods; l++ )
     {
@@ -288,18 +308,65 @@ ODQuadrants;
                     Theta_complete = Theta_wavevector * directionalSpread;
                 }
 
+                mssX  += (kx * kx) * (dkx * dky) * Theta_complete;
+                mssY  += (ky * ky) * (dkx * dky) * Theta_complete;
+                mssXY += (kx * kx + ky * ky) * (dkx * dky) * Theta_complete;
+
                 const float amplitude = sqrtf(2.0f * Theta_complete * dkx * dky);
 
                 baseSpectrum[index] = Theta_complete;
-                H0[index][0] = MATH_1_DIV_SQRT_2f * /*xi_r **/ amplitude * 0.5f;
-                H0[index][1] = MATH_1_DIV_SQRT_2f * /*xi_i **/ amplitude * 0.5f;
+                H0[index][0] = MATH_1_DIV_SQRT_2f * xi_r * amplitude * 0.5f;
+                H0[index][1] = MATH_1_DIV_SQRT_2f * xi_i * amplitude * 0.5f;
 
-                printf("%+f %+fi ", H0[index][0], H0[index][1]);
+                //printf("%+f %+fi ", H0[index][0], H0[index][1]);
             }
 
-            printf("\n");
+            //printf("\n");
         }
     }
+
+    float mss = 0.0f;
+
+    for ( float k = 0.001f; k < 1000.0f; k = k * 1.001f )
+    {
+        // deep water dispersion relation
+        const float omega = sqrtf(k * EARTH_ACCELERATIONf);
+
+        const float kSquare = k * k;
+        const float dk = (k * 1.001f) - k;
+
+        // eq A3
+        const float Theta = energy_donelan_wave_frequency(omega, U10, fetch);
+        const float sk = Theta * 0.5f * (EARTH_ACCELERATIONf / omega);
+
+        // eq A6
+        mss += kSquare * sk * dk;
+    }
+
+    printf("mssx: %f mssy: %f mss: %f mss: %f\n", mssX, mssY, mssXY, mss);
+
+    mss = 0.0f;
+
+    for ( float omega = 0.001f; omega < 100.0f; omega = omega * 1.001f )
+    {
+        const float nextOmega = omega * 1.001f;
+        // deep water dispersion relation
+        const float k = (omega * omega) / EARTH_ACCELERATIONf;
+        const float nextk = (nextOmega * nextOmega) / EARTH_ACCELERATIONf;
+
+        const float kSquare = k * k;
+        const float dk = nextk - k;
+
+        // eq A3
+        const float Theta = energy_donelan_wave_frequency(omega, U10, fetch);
+        const float sk = Theta * 0.5f * (EARTH_ACCELERATIONf / omega);
+
+        // eq A6
+        mss += kSquare * sk * dk;
+    }
+
+
+    printf("mssx: %f mssy: %f mss: %f mss: %f\n", mssX, mssY, mssXY, mss);
 }
 
 - (void) generateUnifiedSpectrum
