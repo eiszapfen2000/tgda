afb215b572285842e400c5d895e796f22dbfcf8d
Thomas Gamper <1222479+eiszapfen2000@users.noreply.github.com>
2015-07-23 15:35:24 +0000

    TG: - generate_k.m handle odd resolutions - generate_xz.m likewise

--- Trunk/Matlab/generate_k.m
+++ Trunk/Matlab/generate_k.m
@@ -1,14 +1,14 @@
-function [k kn deltakx deltaky] = generate_k(resolution, area)
+function [k, kn, deltakx, deltaky] = generate_k(resolution, area)
 
 k = zeros(resolution, resolution, 2);
 
-alphas = -resolution/2:1:resolution/2-1;
-betas  =  resolution/2:-1:-(resolution/2)+1;
+alphas = ceil(-resolution/2): 1:ceil( resolution/2)-1;
+betas  = floor( resolution/2):-1:floor(-resolution/2)+1;
 deltakx = 2*pi / area;
 deltaky = 2*pi / area;
 
 % wave vector domain
-[ k(:,:,1) k(:,:,2) ] = meshgrid(alphas.*deltakx, betas.*deltaky);
+[ k(:,:,1), k(:,:,2) ] = meshgrid(alphas.*deltakx, betas.*deltaky);
 
 kn = sqrt(sum(abs(k).^2, 3));
 
