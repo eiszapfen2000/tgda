8fec8a099eb703faef8f3b8de9e7de2a8641dd94
Thomas Gamper <1222479+eiszapfen2000@users.noreply.github.com>
2015-07-27 13:06:26 +0000

    TG: - imrelnormalize.m fix - writelodimages.m use imrelnormalize - generated_data_images.m shorten

--- Trunk/Matlab/writelodimages.m
+++ Trunk/Matlab/writelodimages.m
@@ -1,11 +1,18 @@
 function writelodimages( prefix, lod )
 
 ppi2meters = ceil((96 / 2.54) * 100);
-imwrite(imnormalize(lod.heights), [ prefix 'heights.png'        ], 'ResolutionUnit', 'meter', 'XResolution', ppi2meters, 'YResolution', ppi2meters);
-imwrite(imnormalize(lod.gx),      [ prefix 'gradient_x.png'     ], 'ResolutionUnit', 'meter', 'XResolution', ppi2meters, 'YResolution', ppi2meters);
-imwrite(imnormalize(lod.gz),      [ prefix 'gradient_z.png'     ], 'ResolutionUnit', 'meter', 'XResolution', ppi2meters, 'YResolution', ppi2meters);
-imwrite(imnormalize(lod.dx),      [ prefix 'displacement_x.png' ], 'ResolutionUnit', 'meter', 'XResolution', ppi2meters, 'YResolution', ppi2meters);
-imwrite(imnormalize(lod.dz),      [ prefix 'displacement_z.png' ], 'ResolutionUnit', 'meter', 'XResolution', ppi2meters, 'YResolution', ppi2meters);
+
+gradients = {lod.gx, lod.gz};
+displacements = {lod.dx, lod.dz};
+
+ngradients = imrelnormalize(gradients);
+ndisplacements = imrelnormalize(displacements);
+
+imwrite(imnormalize(lod.heights), [ prefix 'heights.png' ], 'ResolutionUnit', 'meter', 'XResolution', ppi2meters, 'YResolution', ppi2meters);
+imwrite(ngradients{1},     [ prefix 'gradient_x.png'     ], 'ResolutionUnit', 'meter', 'XResolution', ppi2meters, 'YResolution', ppi2meters);
+imwrite(ngradients{2},     [ prefix 'gradient_z.png'     ], 'ResolutionUnit', 'meter', 'XResolution', ppi2meters, 'YResolution', ppi2meters);
+imwrite(ndisplacements{1}, [ prefix 'displacement_x.png' ], 'ResolutionUnit', 'meter', 'XResolution', ppi2meters, 'YResolution', ppi2meters);
+imwrite(ndisplacements{2}, [ prefix 'displacement_z.png' ], 'ResolutionUnit', 'meter', 'XResolution', ppi2meters, 'YResolution', ppi2meters);
 
 end
 
