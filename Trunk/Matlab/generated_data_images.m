2973cae19f8d5460c8695963257c58d4ba753cc9
icicle <icicle@f832cba2-7736-0410-a208-86f5be76f6c5>
2015-07-27 13:06:26 +0000

    TG: - imrelnormalize.m fix - writelodimages.m use imrelnormalize - generated_data_images.m shorten

--- Trunk/Matlab/generated_data_images.m
+++ Trunk/Matlab/generated_data_images.m
@@ -17,50 +17,41 @@ lolz = ocean_init(geometry, settings);
 lolz = ocean_animate(lolz, time);
 lolz = ocean_transform(lolz);
 
-%writelodimages('u_100_100km_', lolz.lods(1));
-settings.wind = [ 8 0 ];
-
-lalz = ocean_init(geometry, settings);
-lalz = ocean_animate(lalz, time);
-lalz = ocean_transform(lalz);
-
-brak = {lolz.lods(1).heights, lalz.lods(1).heights};
-brok = imrelnormalize(brak);
-
-figure
-subplot(3, 4, 1);
-imshow(brok{1});
-subplot(3, 4, 2);
-imshow(brok{2});
-
-brak = {lolz.lods(1).gx, lalz.lods(1).gx};
-brok = imrelnormalize(brak);
-
-subplot(3, 4, 5);
-imshow(brok{1});
-subplot(3, 4, 6);
-imshow(brok{2});
-
-brak = {lolz.lods(1).gz, lalz.lods(1).gz};
-brok = imrelnormalize(brak);
-
-subplot(3, 4, 7);
-imshow(brok{1});
-subplot(3, 4, 8);
-imshow(brok{2});
-
-brak = {lolz.lods(1).dx, lalz.lods(1).dx};
-brok = imrelnormalize(brak);
-
-subplot(3, 4, 9);
-imshow(brok{1});
-subplot(3, 4, 10);
-imshow(brok{2});
-
-brak = {lolz.lods(1).dz, lalz.lods(1).dz};
-brok = imrelnormalize(brak);
-
-subplot(3, 4, 11);
-imshow(brok{1});
-subplot(3, 4, 12);
-imshow(brok{2});
+writelodimages('u_30_500km_', lolz.lods(1));
+% figure
+% subplot(3, 4, 1);
+% imshow(brok{1});
+% subplot(3, 4, 2);
+% imshow(brok{2});
+% 
+% brak = {lolz.lods(1).gx, lalz.lods(1).gx};
+% brok = imrelnormalize(brak);
+% 
+% subplot(3, 4, 5);
+% imshow(brok{1});
+% subplot(3, 4, 6);
+% imshow(brok{2});
+% 
+% brak = {lolz.lods(1).gz, lalz.lods(1).gz};
+% brok = imrelnormalize(brak);
+% 
+% subplot(3, 4, 7);
+% imshow(brok{1});
+% subplot(3, 4, 8);
+% imshow(brok{2});
+% 
+% brak = {lolz.lods(1).dx, lalz.lods(1).dx};
+% brok = imrelnormalize(brak);
+% 
+% subplot(3, 4, 9);
+% imshow(brok{1});
+% subplot(3, 4, 10);
+% imshow(brok{2});
+% 
+% brak = {lolz.lods(1).dz, lalz.lods(1).dz};
+% brok = imrelnormalize(brak);
+% 
+% subplot(3, 4, 11);
+% imshow(brok{1});
+% subplot(3, 4, 12);
+% imshow(brok{2});
--- Trunk/Matlab/imrelnormalize.m
+++ Trunk/Matlab/imrelnormalize.m
@@ -14,12 +14,10 @@ for i=1:numel(inimages)
     localMax = max(max(inimages{i}));
     globalMin = min([globalMin localMin]);
     globalMax = max([globalMax localMax]);
-    
-    outimages{i} = double(inimages{i}) - localMin;
 end
 
 for i=1:numel(outimages)
-    outimages{i} = outimages{i} ./ (globalMax - globalMin);
+    outimages{i} = (double(inimages{i}) - localMin) ./ (globalMax - globalMin);
 end
 
 end
