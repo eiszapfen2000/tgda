5931a3a966a6b920f8882e81a0c789fe620aa132
Thomas Gamper <1222479+eiszapfen2000@users.noreply.github.com>
2015-07-28 14:50:27 +0000

    TG: - generated_data_images.m some fiddling around with displacement plots

--- Trunk/Matlab/generated_data_images.m
+++ Trunk/Matlab/generated_data_images.m
@@ -6,18 +6,41 @@ time = 1;
 geometry = [];
 geometry.geometryRes = 512;
 geometry.gradientRes = 512;
-geometry.lodAreas = [ 50 ];
+geometry.lodAreas = [ 100 ];
 
 settings = [];
 settings.generatorName = 'Unified';
-settings.wind = [ 30 0 ];
+settings.wind = [ 100 0];
 settings.fetch = 500000;
 
+nplots = 1;
 lolz = ocean_init(geometry, settings);
-lolz = ocean_animate(lolz, time);
-lolz = ocean_transform(lolz);
+figure
 
-writelodimages('u_30_500km_', lolz.lods(1));
+for i=1:nplots
+    time = i * 0.5;
+    lolz = ocean_animate(lolz, time);
+    lolz = ocean_transform(lolz);
+
+    x = lolz.lods(1).x(256,:);
+    h = lolz.lods(1).heights(256,:);
+    dx = lolz.lods(1).dx(256,:);
+    
+    minX = min(x);
+    maxX = max(x);
+    x = [ x (x + (maxX - minX)) ];
+    h = [ h h ];
+    dx = [ dx dx ];
+    
+    subplot(nplots, 1, i);
+    hold on
+    plot(x, h)
+    %axis off
+    plot(x-5.*dx,h,'r')
+    hold off
+end
+
+% writelodimages('u_30_500km_', lolz.lods(1));
 % figure
 % subplot(3, 4, 1);
 % imshow(brok{1});
