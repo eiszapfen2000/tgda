007e12f11ef7ca8704884e4c0e6ee843b62bcb41
icicle <icicle@f832cba2-7736-0410-a208-86f5be76f6c5>
2015-10-14 11:48:11 +0000

    TG: - ocean_init.m use cell array for lods - ocean_animate.m likewise - ocean_transform.m likewise

--- Trunk/Matlab/ocean_transform.m
+++ Trunk/Matlab/ocean_transform.m
@@ -2,41 +2,41 @@ function result = ocean_transform(ocean)
 
 for i=1:numel(ocean.lods)
     
-    resolution = ocean.lods(i).resolution;
+    resolution = ocean.lods{i}.resolution;
     
-    ocean.lods(i).gradient_x = 1i .* ocean.lods(i).k(:,:,1) .* ocean.lods(i).spectrum;
-    ocean.lods(i).gradient_z = 1i .* ocean.lods(i).k(:,:,2) .* ocean.lods(i).spectrum;
-    ocean.lods(i).gradient_x(:,1) = 0;
-    ocean.lods(i).gradient_z(1,:) = 0;
+    ocean.lods{i}.gradient_x = 1i .* ocean.lods{i}.k(:,:,1) .* ocean.lods{i}.spectrum;
+    ocean.lods{i}.gradient_z = 1i .* ocean.lods{i}.k(:,:,2) .* ocean.lods{i}.spectrum;
+    ocean.lods{i}.gradient_x(:,1) = 0;
+    ocean.lods{i}.gradient_z(1,:) = 0;
     
-    dxTerm = -1i .* ocean.lods(i).k(:,:,1) ./ ocean.lods(i).kn;
-    dzTerm = -1i .* ocean.lods(i).k(:,:,2) ./ ocean.lods(i).kn;
+    dxTerm = -1i .* ocean.lods{i}.k(:,:,1) ./ ocean.lods{i}.kn;
+    dzTerm = -1i .* ocean.lods{i}.k(:,:,2) ./ ocean.lods{i}.kn;
     dxTerm(isnan(dxTerm)) = 0;
     dzTerm(isnan(dzTerm)) = 0;
 
-    ocean.lods(i).displacement_x = dxTerm .* ocean.lods(i).spectrum;
-    ocean.lods(i).displacement_z = dzTerm .* ocean.lods(i).spectrum;
-    ocean.lods(i).displacement_x(:,1) = 0;
-    ocean.lods(i).displacement_z(1,:) = 0;
+    ocean.lods{i}.displacement_x = dxTerm .* ocean.lods{i}.spectrum;
+    ocean.lods{i}.displacement_z = dzTerm .* ocean.lods{i}.spectrum;
+    ocean.lods{i}.displacement_x(:,1) = 0;
+    ocean.lods{i}.displacement_z(1,:) = 0;
     
-    ocean.lods(i).displacement_x_x = 1i .* ocean.lods(i).k(:,:,1) .* ocean.lods(i).displacement_x;
-    ocean.lods(i).displacement_x_z = 1i .* ocean.lods(i).k(:,:,2) .* ocean.lods(i).displacement_x;
-    ocean.lods(i).displacement_z_x = 1i .* ocean.lods(i).k(:,:,1) .* ocean.lods(i).displacement_z;
-    ocean.lods(i).displacement_z_z = 1i .* ocean.lods(i).k(:,:,2) .* ocean.lods(i).displacement_z;
-    ocean.lods(i).displacement_x_x(:,1) = 0;
-    ocean.lods(i).displacement_x_z(1,:) = 0;
-    ocean.lods(i).displacement_z_x(:,1) = 0;
-    ocean.lods(i).displacement_z_z(1,:) = 0;
+    ocean.lods{i}.displacement_x_x = 1i .* ocean.lods{i}.k(:,:,1) .* ocean.lods{i}.displacement_x;
+    ocean.lods{i}.displacement_x_z = 1i .* ocean.lods{i}.k(:,:,2) .* ocean.lods{i}.displacement_x;
+    ocean.lods{i}.displacement_z_x = 1i .* ocean.lods{i}.k(:,:,1) .* ocean.lods{i}.displacement_z;
+    ocean.lods{i}.displacement_z_z = 1i .* ocean.lods{i}.k(:,:,2) .* ocean.lods{i}.displacement_z;
+    ocean.lods{i}.displacement_x_x(:,1) = 0;
+    ocean.lods{i}.displacement_x_z(1,:) = 0;
+    ocean.lods{i}.displacement_z_x(:,1) = 0;
+    ocean.lods{i}.displacement_z_z(1,:) = 0;
     
-    ocean.lods(i).heights = real(ifft2(ifftshift(ocean.lods(i).spectrum))) .* (resolution^2);
-    ocean.lods(i).gx = real(ifft2(ifftshift(ocean.lods(i).gradient_x))) .* (resolution^2);
-    ocean.lods(i).gz = real(ifft2(ifftshift(ocean.lods(i).gradient_z))) .* (resolution^2);
-    ocean.lods(i).dx = real(ifft2(ifftshift(ocean.lods(i).displacement_x))) .* (resolution^2);
-    ocean.lods(i).dz = real(ifft2(ifftshift(ocean.lods(i).displacement_z))) .* (resolution^2);
-    ocean.lods(i).dx_x = real(ifft2(ifftshift(ocean.lods(i).displacement_x_x))) .* (resolution^2);
-    ocean.lods(i).dx_z = real(ifft2(ifftshift(ocean.lods(i).displacement_x_z))) .* (resolution^2);
-    ocean.lods(i).dz_x = real(ifft2(ifftshift(ocean.lods(i).displacement_z_x))) .* (resolution^2);
-    ocean.lods(i).dz_z = real(ifft2(ifftshift(ocean.lods(i).displacement_z_z))) .* (resolution^2);
+    ocean.lods{i}.heights = real(ifft2(ifftshift(ocean.lods{i}.spectrum))) .* (resolution^2);
+    ocean.lods{i}.gx = real(ifft2(ifftshift(ocean.lods{i}.gradient_x))) .* (resolution^2);
+    ocean.lods{i}.gz = real(ifft2(ifftshift(ocean.lods{i}.gradient_z))) .* (resolution^2);
+    ocean.lods{i}.dx = real(ifft2(ifftshift(ocean.lods{i}.displacement_x))) .* (resolution^2);
+    ocean.lods{i}.dz = real(ifft2(ifftshift(ocean.lods{i}.displacement_z))) .* (resolution^2);
+    ocean.lods{i}.dx_x = real(ifft2(ifftshift(ocean.lods{i}.displacement_x_x))) .* (resolution^2);
+    ocean.lods{i}.dx_z = real(ifft2(ifftshift(ocean.lods{i}.displacement_x_z))) .* (resolution^2);
+    ocean.lods{i}.dz_x = real(ifft2(ifftshift(ocean.lods{i}.displacement_z_x))) .* (resolution^2);
+    ocean.lods{i}.dz_z = real(ifft2(ifftshift(ocean.lods{i}.displacement_z_z))) .* (resolution^2);
 
     
 %     gradient_x = 1i .* ocean.lods(i).k(:,:,1) .* ocean.lods(i).h_tilde;
