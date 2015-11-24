4ce115062440cc49b4ff2c4516eb78069555d051
icicle <icicle@f832cba2-7736-0410-a208-86f5be76f6c5>
2015-11-24 19:26:33 +0000

    TG: - implementation.tex add plots - wavenumberslod.m generate plot data - write2dcsv.m pimp - sampling_scale*.dat plot data

--- Trunk/Matlab/wavenumberslod.m
+++ Trunk/Matlab/wavenumberslod.m
@@ -1,50 +1,114 @@
+function wavenumberslod
+
 clear
 close all
 
 time = 1;
 igs = 2 / (1 + sqrt(5));
-igs = 1 / (1 + sqrt(5));
-%igs = 1/14;
-%igs = 0.382;
+%igs = 1 / (1 + sqrt(5));
+%igs = 0.5;
 
 geometry = [];
-geometry.geometryRes = 8;
-geometry.gradientRes = 8;
+geometry.geometryRes = 512;
+geometry.gradientRes = 512;
 
 maxArea = 2000;
 geometry.lodAreas = [ maxArea, maxArea*igs maxArea*igs*igs maxArea*igs*igs*igs ];
-%float GRID1_SIZE = 5488.0; // size in meters (i.e. in spatial domain) of the first grid
-%float GRID2_SIZE = 392.0; // size in meters (i.e. in spatial domain) of the second grid
-%float GRID3_SIZE = 28.0; // size in meters (i.e. in spatial domain) of the third grid
-%float GRID4_SIZE = 2.0; // size in meters (i.e. in spatial domain) of the fourth grid
 
 settings = [];
 settings.generatorName = 'Unified';
-settings.wind = [ 10 0 ];
+settings.wind = [ 2.5 0 ];
 settings.fetch = 500000;
 
 o = ocean_init(geometry, settings);
 
-figure
-hold on
-cmap = hsv(numel(o.lods));
-minK = 0;
+maxK = 0;
 for l=1:numel(o.lods)
-    kn = sort(unique(o.lods{l}.kn));
-    %ix = find(kn > minK, 1, 'first');
-    kn = kn(2:end);
-    %size(kn)
-    a = UnifiedSpectrum1Dk(kn, settings.wind, settings.fetch, []);
-    harea = area(kn, a);% 'Color', cmap(l,:));
-    child = get(harea,'Children');
-    set(child,'FaceColor', cmap(l,:))
-    set(child,'FaceAlpha', 1.0 - l*0.1);
-    set(child,'EdgeColor', 'none');    
-    
-    minKx = (pi * o.lods{l}.resolution) / o.lods{l}.area;
-    minK = sqrt(minKx^2 + minKx^2);
+    maxK(end+1) = sqrt(2)* pi * (o.lods{l}.resolution / o.lods{l}.area);
 end
-hold off
+
+for l=1:numel(o.lods)
+    [ kn, a ] = rangedSpectrum(o.lods{l}, 0, 4, settings);
+    write2dcsv(kn', a', sprintf('sampling_scale_06_lod_%d.dat', l));
+end
+
+for l=1:numel(o.lods)
+    [ kn, a ] = rangedSpectrum(o.lods{l}, maxK(l), 4, settings);
+    write2dcsv(kn', a', sprintf('sampling_scale_06_lod_%d_capped.dat', l));
+end
+
+igs = 1 / (1 + sqrt(5));
+geometry.geometryRes = 256;
+geometry.gradientRes = 256;
+geometry.lodAreas = [ maxArea, maxArea*igs maxArea*igs*igs maxArea*igs*igs*igs ];
+o = ocean_init(geometry, settings);
+
+maxK = 0;
+for l=1:numel(o.lods)
+    maxK(end+1) = sqrt(2)* pi * (o.lods{l}.resolution / o.lods{l}.area);
+end
+
+for l=1:numel(o.lods)
+    [ kn, a ] = rangedSpectrum(o.lods{l}, 0, 2, settings);
+    write2dcsv(kn', a', sprintf('sampling_scale_03_lod_%d.dat', l));
+end
+
+for l=1:numel(o.lods)
+    [ kn, a ] = rangedSpectrum(o.lods{l}, maxK(l), 2, settings);
+    write2dcsv(kn', a', sprintf('sampling_scale_03_lod_%d_capped.dat', l));
+end
+
+end
+
+function k = wavenumberrange(kn, minK)
+k = sort(unique(kn));
+ix = find(k > minK, 1, 'first');
+k = k(ix:end);
+end
+
+function [kn, a] = rangedSpectrum(lod, minK, delta, settings)
+    kn = wavenumberrange(lod.kn, minK);
+    kn = kn(1:delta:end);
+    a  = UnifiedSpectrum1Dk(kn, settings.wind, settings.fetch, []);
+end
+
+% figure
+% hold on
+% cmap = hsv(numel(o.lods));
+% minK = 0;
+% maxK = realmax();
+% 
+% for l=1:numel(o.lods)
+%     minKx = (pi * o.lods{l}.resolution) / o.lods{l}.area;
+%     minK(end+1) = sqrt(minKx^2 + minKx^2);
+%     maxK(end+1) = 2*pi/o.lods{l}.area;
+% end
+% 
+% for l=1:numel(o.lods)
+%     kn = sort(unique(o.lods{l}.kn));
+%     ix = find(kn > minK(l), 1, 'first');
+%     kn = kn(ix:end);
+%     %size(kn)
+%     a = UnifiedSpectrum1Dk(kn, settings.wind, settings.fetch, []);
+%     harea = area(kn, a);% 'Color', cmap(l,:));
+%     child = get(harea,'Children');
+%     set(child,'FaceColor', cmap(l,:))
+%     set(child,'FaceAlpha', 1.0 - l*0.1);
+%     set(child,'EdgeColor', 'black'); 
+% end
+% hold off
+% 
+% for l=1:numel(o.lods)
+%     kn = sort(unique(o.lods{l}.kn));
+%     kn = kn(2:end);
+%     a = UnifiedSpectrum1Dk(kn, settings.wind, settings.fetch, []);
+%     figure
+%     harea = area(kn, a);% 'Color', cmap(l,:));
+%     child = get(harea,'Children');
+%     set(child,'FaceColor', cmap(l,:))
+%     set(child,'FaceAlpha', l*0.1);
+%     set(child,'EdgeColor', 'black'); 
+% end
 
 
 
