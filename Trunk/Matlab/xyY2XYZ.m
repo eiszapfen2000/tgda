0e4bab99d475a6426e8c26d640ac8bddd4253455
icicle <icicle@f832cba2-7736-0410-a208-86f5be76f6c5>
2016-03-14 13:43:35 +0000

    TG: - sunRadiance.m add - xyY2XYZ.m handle different dimensionalitites - tonemapReinhard.m compute Lw_white if needed - preethamSky.m partial sundisk work - preetham.m more testing

--- Trunk/Matlab/xyY2XYZ.m
+++ Trunk/Matlab/xyY2XYZ.m
@@ -1,8 +1,23 @@
 function XYZ = xyY2XYZ(xyY)
 
-XYZ = zeros(size(xyY));
-XYZ(:,:,1) = (xyY(:,:,1) ./ xyY(:,:,2)) .* xyY(:,:,3);
-XYZ(:,:,2) = xyY(:,:,3);
-XYZ(:,:,3) = ((1.0 - xyY(:,:,1) - xyY(:,:,2)) ./ xyY(:,:,2)) .* xyY(:,:,3);
+if size(xyY,3) == 3
+    XYZ(:,:,1) = (xyY(:,:,1) ./ xyY(:,:,2)) .* xyY(:,:,3);
+    XYZ(:,:,2) = xyY(:,:,3);
+    XYZ(:,:,3) = ((1.0 - xyY(:,:,1) - xyY(:,:,2)) ./ xyY(:,:,2)) .* xyY(:,:,3);
+else
+    if size(xyY,2) == 3
+        XYZ(:,1) = (xyY(:,1) ./ xyY(:,2)) .* xyY(:,3);
+        XYZ(:,2) = xyY(:,3);
+        XYZ(:,3) = ((1.0 - xyY(:,1) - xyY(:,2)) ./ xyY(:,2)) .* xyY(:,3);
+    else
+        if size(xyY,1) == 3
+            XYZ(1,:) = (xyY(1,:) ./ xyY(2,:)) .* xyY(3,:);
+            XYZ(2,:) = xyY(3,:);
+            XYZ(3,:) = ((1.0 - xyY(1,:) - xyY(2,:)) ./ xyY(2,:)) .* xyY(3,:);
+        else
+            error('Problem?');
+        end
+    end
+end
 
 end
