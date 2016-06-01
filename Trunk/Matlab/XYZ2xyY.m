54e323fd7b6c31865d0b94a74bed772bdde41c32
icicle <icicle@f832cba2-7736-0410-a208-86f5be76f6c5>
2016-06-01 15:36:24 +0000

    TG: - XYZ2lsRGB.m formatting - xyY2XYZ.m handle colors with Y=0 - XYZ2xyY.m handle colors with X=Y=Z=0

--- Trunk/Matlab/XYZ2xyY.m
+++ Trunk/Matlab/XYZ2xyY.m
@@ -1,19 +1,46 @@
-function xyY = XYZ2xyY(XYZ)
+function xyY = XYZ2xyY(XYZ, varargin)
+
+if nargin > 1
+    wp_XYZ = varargin{1};
+else
+    wp_XYZ = whitepoint('D65');
+end
+
+wp_x = wp_XYZ(1) / sum(wp_XYZ);
+wp_y = wp_XYZ(2) / sum(wp_XYZ);
 
 if size(XYZ,3) == 3
-    xyY(:,:,1) = XYZ(:,:,1) ./ sum(XYZ,3);
-    xyY(:,:,2) = XYZ(:,:,2) ./ sum(XYZ,3);
-    xyY(:,:,3) = XYZ(:,:,2);
+    X = XYZ(:,:,1); Y = XYZ(:,:,2); % Z = XYZ(:,:,3);
+    x = ones(size(X)).*wp_x; y = ones(size(X)).*wp_y;
+    s = sum(XYZ,3);
+    nz = s > 0;
+    
+    x(nz) = X(nz) ./ s(nz);
+    y(nz) = Y(nz) ./ s(nz);
+    
+    xyY(:,:,1) = x; xyY(:,:,2) = y; xyY(:,:,3) = Y;    
 else
     if size(XYZ,2) == 3
-        xyY(:,1) = XYZ(:,1) ./ sum(XYZ,2);
-        xyY(:,2) = XYZ(:,2) ./ sum(XYZ,2);
-        xyY(:,3) = XYZ(:,2);
+        X = XYZ(:,1); Y = XYZ(:,2); % Z = XYZ(:,3);
+        x = ones(size(X)).*wp_x; y = ones(size(X)).*wp_y;
+        s = sum(XYZ,2);
+        nz = s > 0;
+
+        x(nz) = X(nz) ./ s(nz);
+        y(nz) = Y(nz) ./ s(nz);
+
+        xyY(:,1) = x; xyY(:,2) = y; xyY(:,3) = Y; 
     else
         if size(XYZ,1) == 3
-            xyY(1,:) = XYZ(1,:) ./ sum(XYZ,1);
-            xyY(2,:) = XYZ(2,:) ./ sum(XYZ,1);
-            xyY(3,:) = XYZ(2,:);
+            X = XYZ(1,:); Y = XYZ(2,:); % Z = XYZ(3,:);
+            x = ones(size(X)).*wp_x; y = ones(size(X)).*wp_y;
+            s = sum(XYZ,1);
+            nz = s > 0;
+
+            x(nz) = X(nz) ./ s(nz);
+            y(nz) = Y(nz) ./ s(nz);
+
+            xyY(1,:) = x; xyY(2,:) = y; xyY(3,:) = Y; 
         else
             error('Problem?');
         end
