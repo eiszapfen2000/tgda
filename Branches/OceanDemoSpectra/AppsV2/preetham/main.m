382081d592b10855965ec68532b4d59da24f85c3
icicle <icicle@f832cba2-7736-0410-a208-86f5be76f6c5>
2016-07-15 09:53:44 +0000

    TG: main.m - use color conversions from NPEngine2

--- Branches/OceanDemoSpectra/AppsV2/preetham/main.m
+++ Branches/OceanDemoSpectra/AppsV2/preetham/main.m
@@ -257,76 +257,6 @@ static Vector3 compute_sun_color(double turbidity, double thetaSun)
     return XYZ;
 }
 
-static Vector3 xyY_to_XYZ(Vector3 xyY)
-{
-    assert(xyY.z > 0.0);
-
-    Vector3 XYZ;
-    XYZ.x = (xyY.x / xyY.y) * xyY.z;
-    XYZ.y = xyY.z;
-    XYZ.z = ((1.0 - xyY.x - xyY.y) / xyY.y) * xyY.z;
-
-    return XYZ;
-}
-
-static Vector3 XYZ_to_xyY(Vector3 XYZ)
-{
-    assert(!(XYZ.x == 0.0 && XYZ.y == 0.0 && XYZ.z ));
-
-    Vector3 xyY;
-    xyY.x = XYZ.x / (XYZ.x + XYZ.y + XYZ.z);
-    xyY.y = XYZ.y / (XYZ.x + XYZ.y + XYZ.z);
-    xyY.z = XYZ.y;
-
-    return xyY;
-}
-
-static Vector3 Lab_to_XYZ(Vector3 Lab, Vector3 RefWhiteXYZ)
-{
-	const double epsilon = 216.0 / 24389.0;
-	const double kappa = 24389.0 / 27.0;
-
-	const double fy = (Lab.x + 16.0) / 116.0;
-	const double fx = (Lab.y / 500.0) + fy;
-	const double fz = fy - (Lab.z / 200.0);
-
-	const double fx3 = pow(fx, 3.0);
-	const double fy3 = pow(fy, 3.0);
-	const double fz3 = pow(fz, 3.0);
-
-	const double xr = (fx3 > epsilon) ? fx3 : ((116.0*fx - 16.0) / kappa);
-	const double yr = (Lab.x > (kappa*epsilon)) ? fy3 : (Lab.x / kappa);
-	const double zr = (fz3 > epsilon) ? fz3 : ((116.0*fz - 16.0) / kappa);
-
-	Vector3 XYZ;
-	XYZ.x = xr * RefWhiteXYZ.x;
-	XYZ.y = yr * RefWhiteXYZ.y;
-	XYZ.z = zr * RefWhiteXYZ.z;
-
-	return XYZ;
-}
-
-static Vector3 XYZ_to_Lab(Vector3 XYZ, Vector3 RefWhiteXYZ)
-{
-	const double epsilon = 216.0 / 24389.0;
-	const double kappa = 24389.0 / 27.0;
-
-	const double xr = XYZ.x / RefWhiteXYZ.x;
-	const double yr = XYZ.y / RefWhiteXYZ.y;
-	const double zr = XYZ.z / RefWhiteXYZ.z;
-
-	const double fx = (xr > epsilon) ? pow(xr, 1.0/3.0) : ((kappa*xr + 16.0) / 116.0);
-	const double fy = (yr > epsilon) ? pow(yr, 1.0/3.0) : ((kappa*yr + 16.0) / 116.0);
-	const double fz = (zr > epsilon) ? pow(zr, 1.0/3.0) : ((kappa*zr + 16.0) / 116.0);
-
-	Vector3 Lab;
-	Lab.x = 116.0 * fy - 16.0;
-	Lab.y = 500.0 * (fx - fy);
-	Lab.z = 200.0 * (fy - fz);
-
-	return Lab;
-}
-
 /**
  * reference white in XYZ coordinates
  */
@@ -794,7 +724,8 @@ int main (int argc, char **argv)
 
 		            double n_dot_v = v3_vv_dot_product(NP_WORLD_Z_AXIS, &v);
 
-		            Vector3 XYZ = xyY_to_XYZ(xyY);
+		            Vector3 XYZ;
+                    xyY_to_XYZ(&xyY, &XYZ);
 		            irradiance_XYZ.x += XYZ.x * phiStep * thetaStep * n_dot_v;
 		            irradiance_XYZ.y += XYZ.y * phiStep * thetaStep * n_dot_v;
 		            irradiance_XYZ.z += XYZ.z * phiStep * thetaStep * n_dot_v;
@@ -814,11 +745,17 @@ int main (int argc, char **argv)
         sun_xyY.y = zenithColor_xyY.y * (nominatorSun.y / denominator.y);
         sun_xyY.z = zenithColor_xyY.z * (nominatorSun.z / denominator.z);
 
-        Vector3 sun_XYZ = xyY_to_XYZ(sun_xyY);
-		Vector3 sun_Lab = XYZ_to_Lab(sun_XYZ, D50);
-		Vector3 sun_XYZ_from_Lab = Lab_to_XYZ(sun_Lab, D50);
+        Vector3 sun_XYZ;
+        xyY_to_XYZ(&sun_xyY, &sun_XYZ);
 
-		Vector3 sunColor_Lab = XYZ_to_Lab(sunColor_XYZ, D50);
+		Vector3 sun_Lab;
+        XYZ_to_Lab(&sun_XYZ, &D50, &sun_Lab);
+
+		Vector3 sun_XYZ_from_Lab;
+        Lab_to_XYZ(&sun_Lab, &D50, &sun_XYZ_from_Lab);
+
+		Vector3 sunColor_Lab;
+        XYZ_to_Lab(&sunColor_XYZ, &D50, &sunColor_Lab);
 
 		Vector3 combined_Lab;
 		//combined_Lab.x = MAX(sunColor_Lab.x, sun_Lab.x*2.0);
@@ -826,7 +763,8 @@ int main (int argc, char **argv)
 		combined_Lab.y = sun_Lab.y;
 		combined_Lab.z = sun_Lab.z;
 
-		Vector3 combined_XYZ = Lab_to_XYZ(combined_Lab, D50);
+		Vector3 combined_XYZ;
+        Lab_to_XYZ(&combined_Lab, &D50, &combined_XYZ);
 
         const FVector3 A = { ABCDE_x[0], ABCDE_y[0], ABCDE_Y[0] };
         const FVector3 B = { ABCDE_x[1], ABCDE_y[1], ABCDE_Y[1] };
