4ba51c73df30b7b985a7e242084451d254bf38a9
icicle <icicle@f832cba2-7736-0410-a208-86f5be76f6c5>
2016-07-15 10:04:08 +0000

    TG: ColorConversions - fix conversion matrix names

--- Branches/OceanDemoSpectra/NPEngine2/Core/Color/ColorConversions.h
+++ Branches/OceanDemoSpectra/NPEngine2/Core/Color/ColorConversions.h
@@ -8,8 +8,8 @@ void npcolor_colorconversions_initialise(void);
 
 extern Matrix3 * NP_LINEAR_sRGB_D65_TO_XYZ;
 extern Matrix3 * NP_LINEAR_sRGB_D50_TO_XYZ;
-extern Matrix3 * NP_XYZ_TO_LINEAR_sRGB_65;
-extern Matrix3 * NP_XYZ_TO_LINEAR_sRGB_50;
+extern Matrix3 * NP_XYZ_TO_LINEAR_sRGB_D65;
+extern Matrix3 * NP_XYZ_TO_LINEAR_sRGB_D50;
 
 void xyY_to_XYZ(const Vector3 * const xyY, Vector3 * XYZ);
 void XYZ_to_xyY(const Vector3 * const XYZ, Vector3 * xyY);
