e6f851592684369c8e213b152d8198ebdd1302e4
icicle <icicle@f832cba2-7736-0410-a208-86f5be76f6c5>
2017-04-13 14:27:40 +0000

    - lsRGB2sRGB.m fit input into range [0...1]

--- Trunk/Matlab/lsRGB2sRGB.m
+++ Trunk/Matlab/lsRGB2sRGB.m
@@ -1,13 +1,24 @@
 function sRGB = lsRGB2sRGB(lsRGB)
 
+% input may be outside the nominal range [0...1]
+mask = (lsRGB < 0);
+
+% clamp negative values to zero
+if any(mask)
+	lsRGB(mask) = 0;
+end
+
+% if values are larger than 1, rescale
+max_lsRGB = max(max(max(lsRGB)));
+
+if (max_lsRGB > 1)
+	lsRGB = lsRGB ./ max_lsRGB;
+end
+
 % convert to non-linear sRGB 
 sRGB = lsRGB;
 mask = (sRGB > 0.0031308);
 sRGB(mask) = 1.055 * (sRGB(mask) .^ (1 / 2.4)) - 0.055;
 sRGB(~mask) = 12.92 * sRGB(~mask);
-
-if max(max(max(lsRGB))) > 1
-    error('Linear RGB values not in range [0,1]');
-end
 
 end
