50d6ff1dcb8da2fa8c07afa55177e1da28de0f1b
Thomas Gamper <1222479+eiszapfen2000@users.noreply.github.com>
2017-07-31 11:44:12 +0000

    - lsRGB2sRGB.m convert to uint8 - lsRGB2XYZ.m add

--- Trunk/Matlab/lsRGB2sRGB.m
+++ Trunk/Matlab/lsRGB2sRGB.m
@@ -21,4 +21,6 @@ mask = (sRGB > 0.0031308);
 sRGB(mask) = 1.055 * (sRGB(mask) .^ (1 / 2.4)) - 0.055;
 sRGB(~mask) = 12.92 * sRGB(~mask);
 
+% convert to uint8 0...255
+sRGB = uint8(sRGB .* 255);
 end
