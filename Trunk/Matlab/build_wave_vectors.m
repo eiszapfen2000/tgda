c07af86a533e6c17c2a2ab684c80a825629d91c3
icicle <icicle@f832cba2-7736-0410-a208-86f5be76f6c5>
2018-03-10 07:59:34 +0000

    - build_wave_vectors.m formatting

--- Trunk/Matlab/build_wave_vectors.m
+++ Trunk/Matlab/build_wave_vectors.m
@@ -1,4 +1,4 @@
-function [k knorm x] = build_wave_vectors(resolution, area)
+function [k, knorm, x] = build_wave_vectors(resolution, area)
 
 x = zeros(resolution(1), resolution(2), 2);
 k = zeros(resolution(1), resolution(2), 2);
@@ -11,9 +11,9 @@ deltakx = 2*pi / area(1);
 deltaky = 2*pi / area(2);
 
 % spatial domain
-[ x(:,:,1) x(:,:,2) ] = meshgrid(alphas.*deltax, betas.*deltay);
+[ x(:,:,1), x(:,:,2) ] = meshgrid(alphas.*deltax, betas.*deltay);
 % wave vector domain
-[ k(:,:,1) k(:,:,2) ] = meshgrid(alphas.*deltakx, betas.*deltaky);
+[ k(:,:,1), k(:,:,2) ] = meshgrid(alphas.*deltakx, betas.*deltaky);
 
 knorm = sqrt(sum(abs(k).^2, 3));
 
