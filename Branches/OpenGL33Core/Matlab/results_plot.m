8ab46f24a5210c65ceba9477a5fb36fda77b0667
Thomas Gamper <1222479+eiszapfen2000@users.noreply.github.com>
2018-06-19 14:08:20 +0000

    - results_plot.m cleanup

--- Branches/OpenGL33Core/Matlab/results_plot.m
+++ Branches/OpenGL33Core/Matlab/results_plot.m
@@ -13,39 +13,20 @@ filename_sea = [ filename_prefix '_sea' ];
 filename_sky = [ filename_prefix '_sky' ];
 filename_whitecaps = [ filename_prefix '_whitecaps' ];
 
-lsRGB_complete = readPFM([filename_complete '.pfm']);
-lsRGB_grid = readPFM([filename_grid '.pfm']);
-lsRGB_ross = readPFM([filename_ross '.pfm']);
-lsRGB_sea = readPFM([filename_sea '.pfm']);
-lsRGB_sky = readPFM([filename_sky '.pfm']);
-lsRGB_whitecaps = readPFM([filename_whitecaps '.pfm']);
+result_tonemap(filename_complete);
+result_tonemap(filename_grid);
+result_tonemap(filename_ross);
+result_tonemap(filename_sea);
+result_tonemap(filename_sky);
+result_tonemap(filename_whitecaps);
 
-xyY_complete = XYZ2xyY(lsRGB2XYZ(lsRGB_complete));
-xyY_grid = XYZ2xyY(lsRGB2XYZ(lsRGB_grid));
-xyY_ross = XYZ2xyY(lsRGB2XYZ(lsRGB_ross));
-xyY_sea = XYZ2xyY(lsRGB2XYZ(lsRGB_sea));
-xyY_sky = XYZ2xyY(lsRGB2XYZ(lsRGB_sky));
-xyY_whitecaps = XYZ2xyY(lsRGB2XYZ(lsRGB_whitecaps));
+end
 
-xyY_tonemapped_complete = tonemapReinhard(xyY_complete, 0.18, -1);
-xyY_tonemapped_grid = tonemapReinhard(xyY_grid, 0.18, -1);
-xyY_tonemapped_ross = tonemapReinhard(xyY_ross, 0.18, -1);
-xyY_tonemapped_sea = tonemapReinhard(xyY_sea, 0.18, -1);
-xyY_tonemapped_sky = tonemapReinhard(xyY_sky, 0.18, -1);
-xyY_tonemapped_whitecaps = tonemapReinhard(xyY_whitecaps, 0.18, -1);
-
-sRGB_tonemapped_complete = lsRGB2sRGB(XYZ2lsRGB(xyY2XYZ(xyY_tonemapped_complete)));
-sRGB_tonemapped_grid = lsRGB2sRGB(XYZ2lsRGB(xyY2XYZ(xyY_tonemapped_grid)));
-sRGB_tonemapped_ross = lsRGB2sRGB(XYZ2lsRGB(xyY2XYZ(xyY_tonemapped_ross)));
-sRGB_tonemapped_sea = lsRGB2sRGB(XYZ2lsRGB(xyY2XYZ(xyY_tonemapped_sea)));
-sRGB_tonemapped_sky = lsRGB2sRGB(XYZ2lsRGB(xyY2XYZ(xyY_tonemapped_sky)));
-sRGB_tonemapped_whitecaps = lsRGB2sRGB(XYZ2lsRGB(xyY2XYZ(xyY_tonemapped_whitecaps)));
-
-imwrite(sRGB_tonemapped_complete,[filename_complete '.png']);
-imwrite(sRGB_tonemapped_grid,[filename_grid '.png']);
-imwrite(sRGB_tonemapped_ross,[filename_ross '.png']);
-imwrite(sRGB_tonemapped_sea,[filename_sea '.png']);
-imwrite(sRGB_tonemapped_sky,[filename_sky '.png']);
-imwrite(sRGB_tonemapped_whitecaps,[filename_whitecaps '.png']);
-
+function result_tonemap(basename)
+%%
+lsRGB_im = readPFM([basename '.pfm']);
+xyY_im = XYZ2xyY(lsRGB2XYZ(lsRGB_im));
+xyY_im_tonemapped = tonemapReinhard(xyY_im, 0.18, -1);
+sRGB_im_tonemapped = lsRGB2sRGB(XYZ2lsRGB(xyY2XYZ(xyY_im_tonemapped)));
+imwrite(sRGB_im_tonemapped,[basename '.png']);
 end
