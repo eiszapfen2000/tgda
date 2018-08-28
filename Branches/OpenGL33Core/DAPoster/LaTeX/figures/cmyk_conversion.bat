54bc8126fb11acd19a2d2b353412787f8c232f25
Thomas Gamper <1222479+eiszapfen2000@users.noreply.github.com>
2018-08-28 09:20:44 +0000

    - cmyk_conversion.bat latex does not support tif, need jpg

--- Branches/OpenGL33Core/DAPoster/LaTeX/figures/cmyk_conversion.bat
+++ Branches/OpenGL33Core/DAPoster/LaTeX/figures/cmyk_conversion.bat
@@ -1,25 +1,25 @@
-convert 20-06-2018_14-49-57_complete.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc 20-06-2018_14-49-57_complete_cmyk.tif
-convert 21-06-2018_10-44-51_complete.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc 21-06-2018_10-44-51_complete_cmyk.tif
-convert 21-06-2018_12-48-51_complete.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc 21-06-2018_12-48-51_complete_cmyk.tif
-convert 21-06-2018_13-45-06_complete.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc 21-06-2018_13-45-06_complete_cmyk.tif
-convert 21-06-2018_13-45-25_complete.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc 21-06-2018_13-45-25_complete_cmyk.tif
-convert 21-06-2018_15-47-53_complete.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc 21-06-2018_15-47-53_complete_cmyk.tif
-convert 28-05-2018_10-56-10_complete.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc 28-05-2018_10-56-10_complete_cmyk.tif
-convert 28-05-2018_10-56-10_grid.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc 28-05-2018_10-56-10_grid_cmyk.tif
-convert 28-05-2018_10-56-10_ross.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc 28-05-2018_10-56-10_ross_cmyk.tif
-convert 28-05-2018_10-56-10_sea.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc 28-05-2018_10-56-10_sea_cmyk.tif
-convert 28-05-2018_10-56-10_sky.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc 28-05-2018_10-56-10_sky_cmyk.tif
-convert 28-05-2018_10-56-10_whitecaps.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc 28-05-2018_10-56-10_whitecaps_cmyk.tif
-convert 186-2.tif +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc 186-2_cmyk.tif
+convert 20-06-2018_14-49-57_complete.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc -quality 100 20-06-2018_14-49-57_complete_cmyk.jpg
+convert 21-06-2018_10-44-51_complete.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc -quality 100 21-06-2018_10-44-51_complete_cmyk.jpg
+convert 21-06-2018_12-48-51_complete.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc -quality 100 21-06-2018_12-48-51_complete_cmyk.jpg
+convert 21-06-2018_13-45-06_complete.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc -quality 100 21-06-2018_13-45-06_complete_cmyk.jpg
+convert 21-06-2018_13-45-25_complete.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc -quality 100 21-06-2018_13-45-25_complete_cmyk.jpg
+convert 21-06-2018_15-47-53_complete.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc -quality 100 21-06-2018_15-47-53_complete_cmyk.jpg
+convert 28-05-2018_10-56-10_complete.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc -quality 100 28-05-2018_10-56-10_complete_cmyk.jpg
+convert 28-05-2018_10-56-10_grid.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc -quality 100 28-05-2018_10-56-10_grid_cmyk.jpg
+convert 28-05-2018_10-56-10_ross.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc -quality 100 28-05-2018_10-56-10_ross_cmyk.jpg
+convert 28-05-2018_10-56-10_sea.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc -quality 100 28-05-2018_10-56-10_sea_cmyk.jpg
+convert 28-05-2018_10-56-10_sky.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc -quality 100 28-05-2018_10-56-10_sky_cmyk.jpg
+convert 28-05-2018_10-56-10_whitecaps.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc -quality 100 28-05-2018_10-56-10_whitecaps_cmyk.jpg
+convert 186-2.jpg +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc -quality 100 186-2_cmyk.jpg
 
-convert u_15_500km_displacement_x.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc u_15_500km_displacement_x_cmyk.tif
-convert u_15_500km_displacement_z.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc u_15_500km_displacement_z_cmyk.tif
-convert u_15_500km_gradient_x.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc u_15_500km_gradient_x_cmyk.tif
-convert u_15_500km_gradient_z.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc u_15_500km_gradient_z_cmyk.tif
-convert u_15_500km_heights.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc u_15_500km_heights_cmyk.tif
+convert u_15_500km_displacement_x.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc -quality 100 u_15_500km_displacement_x_cmyk.jpg
+convert u_15_500km_displacement_z.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc -quality 100 u_15_500km_displacement_z_cmyk.jpg
+convert u_15_500km_gradient_x.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc -quality 100 u_15_500km_gradient_x_cmyk.jpg
+convert u_15_500km_gradient_z.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc -quality 100 u_15_500km_gradient_z_cmyk.jpg
+convert u_15_500km_heights.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc -quality 100 u_15_500km_heights_cmyk.jpg
 
-convert u_30_500km_displacement_x.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc u_30_500km_displacement_x_cmyk.tif
-convert u_30_500km_displacement_z.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc u_30_500km_displacement_z_cmyk.tif
-convert u_30_500km_gradient_x.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc u_30_500km_gradient_x_cmyk.tif
-convert u_30_500km_gradient_z.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc u_30_500km_gradient_z_cmyk.tif
-convert u_30_500km_heights.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc u_30_500km_heights_cmyk.tif
+convert u_30_500km_displacement_x.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc -quality 100 u_30_500km_displacement_x_cmyk.jpg
+convert u_30_500km_displacement_z.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc -quality 100 u_30_500km_displacement_z_cmyk.jpg
+convert u_30_500km_gradient_x.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc -quality 100 u_30_500km_gradient_x_cmyk.jpg
+convert u_30_500km_gradient_z.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc -quality 100 u_30_500km_gradient_z_cmyk.jpg
+convert u_30_500km_heights.png +profile icm -profile sRGB.icc -profile CoatedFOGRA27.icc -quality 100 u_30_500km_heights_cmyk.jpg
